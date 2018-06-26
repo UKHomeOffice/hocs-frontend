53c9655fc35abb30605eafce7736a8f1221a710e
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-06-26 08:22:55 +0100

    refactor api

--- server/routes/forms.js
+++ server/routes/forms.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const actionRouter = require('./action/index');
 const caseRouter = require('./case/index');
+const formRouter = require('./forms/index');
 
+router.use('/forms', formRouter);
 router.use('/action', actionRouter);
-
 router.use('/case', caseRouter);
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,8 @@ const assets = require('../../build/assets.json');
 const html = require('../layout/html');
 const authMiddleware = require('../middleware/auth');
 const renderMiddleware = require('../middleware/render');
-const apiRouter = require('./api');
+const formsRouter = require('./forms');
 const {getFormForCase, getFormForAction} = require('../services/form');
-const User = require('../models/user');
 
 html.use(assets);
 
@@ -19,29 +18,7 @@ router.use('/case/:type/:action', (req, res, next) => {
     getFormForCase(req, res, next);
 });
 
-router.use('/api/action/:action', (req, res, next) => {
-    getFormForAction(req, res, next);
-});
-
-router.use('/api/case/:type/:action', (req, res, next) => {
-    getFormForCase(req, res, next);
-});
-
-router.get('/api/action/:action', (req, res, next) => {
-    const {action} = req.params;
-    if (req.user && User.hasRole(req.user, action.toUpperCase())) {
-        res.status(200).send(req.form.schema);
-    } else {
-        res.status(403).send();
-    }
-});
-
-router.get('/api/case/:type/:action', (req, res) => {
-    res.status(200).send(req.form.schema);
-
-});
-
-router.use('/', apiRouter);
+router.use('/', formsRouter);
 
 router.use('*', renderMiddleware);
 
