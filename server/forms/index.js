1555487771d8514f8dd0ae058391332726e46fae
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2018-07-09 18:24:23 +0100

    Add bulk upload form

--- server/forms/index.js
+++ server/forms/index.js
@@ -1,16 +1,18 @@
 const caseCreate = require('./case-create.json');
 const addDocument = require('./document-add.json');
 const testForm = require('./case-test.json');
+const bulkCreate = require('./bulk-create.json');
 
 const forms = {
     caseCreate: caseCreate,
     addDocument: addDocument,
-    testForm: testForm
+    testForm: testForm,
+    bulkCreate: bulkCreate
 };
 
 module.exports = {
     getForm: (form) => {
-        if (typeof forms[forms] === 'undefined') {
+        if (typeof forms[form] === 'undefined') {
             throw new ReferenceError(`Form ${form} does not exist`);
         }
         return JSON.parse(JSON.stringify(forms[form]));
