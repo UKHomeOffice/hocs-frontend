dffd65609e9527b900fc4129560d2cd8ae045bc9
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2018-07-10 18:35:02 +0100

    Error more helpfully on bad form syntax

--- server/forms/index.js
+++ server/forms/index.js
@@ -1,13 +1,20 @@
 const caseCreate = require('./case-create.json');
 const addDocument = require('./document-add.json');
 const testForm = require('./case-test.json');
+const bulkCreate = require('./bulk-create.json');
 
 const forms = {
     caseCreate: caseCreate,
     addDocument: addDocument,
-    testForm: testForm
+    testForm: testForm,
+    bulkCreate: bulkCreate
 };
 
 module.exports = {
-    getForm: (form) => JSON.parse(JSON.stringify(forms[form]))
+    getForm: (form) => {
+        if (typeof forms[form] === 'undefined') {
+            throw new ReferenceError(`Form ${form} does not exist`);
+        }
+        return JSON.parse(JSON.stringify(forms[form]));
+    }
 };
