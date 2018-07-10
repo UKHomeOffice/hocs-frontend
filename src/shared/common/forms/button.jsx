a749e94bd0cbf71a1732f3bf4d5441d610fce7b0
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-10 11:08:30 +0100

    Fix back button behaviour on forms

--- src/shared/common/forms/button.jsx
+++ src/shared/common/forms/button.jsx
@@ -1,37 +1,53 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import {ApplicationConsumer} from '../../contexts/application.jsx';
+import {cancel} from '../../contexts/actions/index.jsx';
 
 class Submit extends Component {
 
-  render() {
-    const {
-      label,
-      className,
-      disabled,
-      action
-    } = this.props;
-    return (
-      <Fragment>
-        <Link
-          className={`button${className ? ' ' + className : ''}`}
-          disabled={disabled}
-          to={action}
-        >{label}</Link>
-      </Fragment>
-    )
-  }
+    handleClick(e) {
+        e.preventDefault();
+        this.props.dispatch(cancel());
+    }
+
+    render() {
+        const {
+            label,
+            className,
+            disabled,
+            action
+        } = this.props;
+        return (
+            <Fragment>
+                <Link
+                    className={`button${className ? ' ' + className : ''}`}
+                    disabled={disabled}
+                    to={action}
+                    onClick={e => this.handleClick(e)}
+                >{label}</Link>
+            </Fragment>
+        );
+    }
 }
 
+Submit.propTypes = {
+    label: PropTypes.string,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    action: PropTypes.func,
+    dispatch: PropTypes.func
+};
+
 Submit.defaultProps = {
-  label: 'Submit',
-  disabled: false
+    label: 'Submit',
+    disabled: false
 };
 
 const WrappedButton = props => (
-  <ApplicationConsumer>
-      {({dispatch}) => <Submit {...props} dispatch={dispatch}/>}
-  </ApplicationConsumer>
+    <ApplicationConsumer>
+        {({dispatch}) => <Submit {...props} dispatch={dispatch}/>}
+    </ApplicationConsumer>
 );
 
 export default WrappedButton;
