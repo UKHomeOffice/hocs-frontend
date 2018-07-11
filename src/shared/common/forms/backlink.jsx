630063dbe09dc1b42632b3648b051bbe8a0438a5
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2018-07-11 11:14:29 +0100

    Add missing JSX file

---
+++ src/shared/common/forms/backlink.jsx
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { ApplicationConsumer } from '../../contexts/application.jsx';
+import { cancel } from '../../contexts/actions/index.jsx';
+
+class BackLink extends Component {
+
+    handleClick(e) {
+        e.preventDefault();
+        this.props.dispatch(cancel());
+    }
+
+    render() {
+        const {
+            to,
+            className,
+            label
+        } = this.props;
+        return (
+            <p>
+                <Fragment>
+                    <Link
+                        className={`link-back ${className ? ' ' + className : ''}`}
+                        to={to}
+                        onClick={e => this.handleClick(e)}
+                    >{label}</Link>
+
+                </Fragment>
+            </p>
+        );
+    }
+}
+
+BackLink.propTypes = {
+    to: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    dispatch: PropTypes.func.isRequired,
+    label: PropTypes.string,
+    isAllowedInFooter: PropTypes.bool,
+};
+
+BackLink.defaultProps = {
+    disabled: false,
+    to: '/',
+    label: 'Back',
+    isAllowedInFooter: PropTypes.bool
+};
+
+const WrappedButton = props => (
+    <ApplicationConsumer>
+        {({ dispatch }) => <BackLink {...props} dispatch={dispatch}/>}
+    </ApplicationConsumer>
+);
+
+export default WrappedButton;
