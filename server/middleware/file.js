9499f80717b98905e311c41393890d8a0824ea4f
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-19 11:27:07 +0100

    Add configuration for SSE on untrusted bucket

--- server/middleware/file.js
+++ server/middleware/file.js
@@ -1,8 +1,14 @@
 const { s3 } = require('../libs/aws');
-const { S3: { BUCKET_NAME } } = require('../config').forContext('AWS');
+const isProduction = require('../config').isProduction;
+const { S3: { BUCKET_NAME, SSE_KEY } } = require('../config').forContext('AWS');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const uuid = require('uuid/v4');
+
+const sseConfiguration = {
+    serverSideEncryption: 'aws:kms',
+    key: SSE_KEY
+};
 
 const storage = multerS3({
     s3,
@@ -12,7 +18,8 @@ const storage = multerS3({
     },
     key: (req, file, callback) => {
         callback(null, uuid());
-    }
+    },
+    ...(isProduction ? sseConfiguration : {})
 });
 
 const upload = multer({ storage });
