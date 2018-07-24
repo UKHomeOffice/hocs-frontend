fdb53ebf54e9cfa374b44bbe0413070f461b89b6
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-24 13:47:05 +0100

    Implement error model in to form middlewares

--- server/routes/action/index.js
+++ server/routes/action/index.js
@@ -3,25 +3,32 @@ const actionService = require('../../services/action');
 const fileMiddleware = require('../../middleware/file');
 const processMiddleware = require('../../middleware/process');
 const validationMiddleware = require('../../middleware/validation').validator;
+const ErrorModel = require('../../models/error');
 
 router.post(['/:workflow/:context/:action', '/:workflow/:action'], fileMiddleware.any(), processMiddleware, validationMiddleware);
 
-router.post(['/:workflow/:context/:action', '/:workflow/:action'], (req, res, next) => {
+router.post(['/:workflow/:context/:action', '/:workflow/:action'], async (req, res, next) => {
     if (Object.keys(req.form.errors).length > 0) {
         return next();
     }
     const { workflow, context, action } = req.params;
     const { form, user } = req;
-    actionService.performAction( 'ACTION', { workflow, context, action, form, user }, (callbackUrl, err) => {
-        if (err) {
-            return res.redirect('/error');
-        } else {
-            if (res.noScript) {
-                return res.redirect(callbackUrl);
-            }
-            return res.status(200).send({ redirect: callbackUrl, response: {} });
+    const response = await actionService.performAction('ACTION', { workflow, context, action, form, user });
+    const { error, callbackUrl } = response;
+    if (error) {
+        req.error = new ErrorModel({
+            status: 500,
+            title: 'Error',
+            summary: 'Failed to perform action',
+            stackTrace: error.message
+        }).toJson();
+        return next();
+    } else {
+        if (res.noScript) {
+            return res.redirect(callbackUrl);
         }
-    });
+        return res.status(200).send({ redirect: callbackUrl, response: {} });
+    }
 });
 
 module.exports = router;
--- server/routes/case/index.js
+++ server/routes/case/index.js
@@ -3,25 +3,32 @@ const actionService = require('../../services/action');
 const processMiddleware = require('../../middleware/process');
 const fileMiddleware = require('../../middleware/file');
 const validationMiddleware = require('../../middleware/validation').validator;
+const ErrorModel = require('../../models/error');
 
 router.post('/:caseId/:entity/:action', fileMiddleware.any(), processMiddleware, validationMiddleware);
 
-router.post('/:caseId/:entity/:action', (req, res, next) => {
+router.post('/:caseId/:entity/:action', async (req, res, next) => {
     if (Object.keys(req.form.errors).length > 0) {
         return next();
     }
     const { caseId, entity, action } = req.params;
     const { form, user } = req;
-    actionService.performAction('CASE', { caseId, entity, action, form, user }, (callbackUrl, err) => {
-        if (err) {
-            return res.redirect('/error');
-        } else {
-            if (res.noScript) {
-                return res.redirect(callbackUrl);
-            }
-            return res.status(200).send({ redirect: callbackUrl, response: {} });
+    const response = await actionService.performAction('CASE', { caseId, entity, action, form, user });
+    const { error, callbackUrl } = response;
+    if (error) {
+        req.error = new ErrorModel({
+            status: 500,
+            title: 'Error',
+            summary: 'Failed to perform action',
+            stackTrace: error.message
+        }).toJson();
+        return next();
+    } else {
+        if (res.noScript) {
+            return res.redirect(callbackUrl);
         }
-    });
+        return res.status(200).send({ redirect: callbackUrl, response: {} });
+    }
 });
 
 module.exports = router;
