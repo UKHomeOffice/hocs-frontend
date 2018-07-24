5d3bc10b49a2e99a2a8d5598d5f4fe2924dcc9f4
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-24 14:43:33 +0100

    Move error page component up to the Layout component

--- src/shared/layouts/components/body.jsx
+++ src/shared/layouts/components/body.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PhaseBanner from '../../common/components/phase-banner.jsx';
-import Error from '../error.jsx';
 
 class Body extends Component {
     render() {
         const {
             children,
-            error,
             phaseBanner
         } = this.props;
         return (
             <div className="govuk-width-container">
                 {phaseBanner.isVisible && <PhaseBanner {...phaseBanner} />}
                 <main className="govuk-main-wrapper " id="main-content" role="main">
-                    {error ? <Error {...error}/> : children}
+                    {children}
                 </main>
             </div>
         );
@@ -23,7 +21,6 @@ class Body extends Component {
 
 Body.propTypes = {
     children: PropTypes.node,
-    error: PropTypes.object,
     phaseBanner: PropTypes.object
 };
 
