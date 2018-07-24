35c3ec2d08741f5e8dde3d4c2d06cc5ed487f52c
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-07-24 09:48:50 +0100

    Fix lint issues

--- src/shared/layouts/error.jsx
+++ src/shared/layouts/error.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ApplicationConsumer } from '../contexts/application.jsx';
 
 class Error extends Component {
     UNSAFE_componentWillMount() {
