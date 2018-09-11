4cb1651c853e8c488d9e7887fcf1935655e0c280
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-09-12 00:16:44 +0100

    Add Documents

--- src/shared/common/components/document.jsx
+++ src/shared/common/components/document.jsx
@@ -7,7 +7,7 @@ class DocumentPreview extends Component {
         const { activeDocument, caseId } = this.props;
         return (
             <embed
-                src={`/api/case/${caseId}/document/${activeDocument}`}
+                src={`/case/${caseId}/document/${activeDocument}`}
                 type='application/pdf'
                 width='100%' height='100%'
                 alt='pdf'
