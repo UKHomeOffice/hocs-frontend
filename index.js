eb7a466ff288acf4f3035230cdc7ae34e74c1dfc
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-11-27 12:42:16 +0000

    Integration Issues

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const spdy = require('spdy');
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const applicationRouter = require('./server/routes/index');
 const logger = require('./server/libs/logger');
@@ -15,25 +13,7 @@ app.use('/public', express.static(path.join(__dirname, 'build', 'public'), { max
 listService.initialise();
 app.use('/', applicationRouter);
 
-const isProduction = process.env.NODE_ENV === 'production';
-let options;
-
-if(isProduction) {
-    options = {
-        cert: fs.readFileSync('/certs/tls.pem'),
-        key: fs.readFileSync('/certs/tls-key.pem'),
-    };
-    logger.info('Production: using SPDY/H2 and SSL if possible');
-} else {
-    options = {
-        spdy: {
-            ssl: false,
-            plain: true
-        }
-    };
-    logger.info('Not production: Using HTTP/1.1');
-}
-
-spdy.createServer(options, app).listen(port, () => {
+app.listen(port, () => {
     logger.info(`Application listening on port ${port}`);
 });
+
