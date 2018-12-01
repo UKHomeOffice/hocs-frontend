f614dfe36a5d0ca25ac49c8e6d1b288cea3058bf
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-12-01 12:50:06 +0000

    move links to nav bar

--- src/shared/pages/main.jsx
+++ src/shared/pages/main.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Dashboard from '../pages/dashboard.jsx';
 
 class MainPage extends Component {
@@ -12,16 +11,6 @@ class MainPage extends Component {
         return (
             <div className="govuk-grid-row">
                 <div className="govuk-grid-column-full">
-                    <h1 className="govuk-heading-l">
-                        My actions
-                    </h1>
-                    <ul className="govuk-list">
-                        <li><Link to={'/action/create/workflow'}>Create single case</Link></li>
-                        <li><Link to={'/action/bulk/workflow'}>Create cases in bulk</Link></li>
-                        <li><Link to={'/action/standard_line/add'}>Add standard line</Link></li>
-                        <li><Link to={'/action/template/add'}>Add template</Link></li>
-                        <li><Link to={'/action/test/form'}>View test form</Link></li>
-                    </ul>
                     <Dashboard />
                 </div>
             </div>
