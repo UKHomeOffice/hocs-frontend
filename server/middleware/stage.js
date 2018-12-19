fbe2afc725c165953764057e01f2a80aee13d83c
lucascairns <lucas.cairns@digital.homeoffice.gov.uk>
2018-12-19 10:25:59 +0000

    Update headers

--- server/middleware/stage.js
+++ server/middleware/stage.js
@@ -6,15 +6,8 @@ const { caseworkServiceClient } = require('../libs/request');
 async function stageResponseMiddleware(req, res, next) {
     const { caseId, stageId } = req.params;
     const { form, user } = req;
-    let headers = {
-        headers: {
-            'X-Auth-UserId': user.id,
-            'X-Auth-Roles': user.roles.join(),
-            'X-Auth-Groups': user.groups.join()
-        }
-    };
     try {
-        const response = await actionService.performAction('WORKFLOW', { caseId, stageId, form, user }, headers);
+        const response = await actionService.performAction('WORKFLOW', { caseId, stageId, form, user }, { headers: User.createHeaders(user) });
         const { callbackUrl } = response;
         return res.redirect(callbackUrl);
     } catch (e) {
@@ -27,15 +20,8 @@ async function stageResponseMiddleware(req, res, next) {
 async function stageApiResponseMiddleware(req, res, next) {
     const { caseId, stageId } = req.params;
     const { form, user } = req;
-    let headers = {
-        headers: {
-            'X-Auth-UserId': user.id,
-            'X-Auth-Roles': user.roles.join(),
-            'X-Auth-Groups': user.groups.join()
-        }
-    };
     try {
-        const response = await actionService.performAction('WORKFLOW', { caseId, stageId, form, user }, headers);
+        const response = await actionService.performAction('WORKFLOW', { caseId, stageId, form, user }, { headers: User.createHeaders(user) });
         const { callbackUrl } = response;
         return res.status(200).json({ redirect: callbackUrl });
     } catch (e) {
@@ -46,17 +32,10 @@ async function stageApiResponseMiddleware(req, res, next) {
 async function allocateCase(req, res, next) {
     const { caseId, stageId } = req.params;
     const { user } = req;
-    let headers = {
-        headers: {
-            'X-Auth-UserId': user.uuid,
-            'X-Auth-Roles': user.roles.join(),
-            'X-Auth-Groups': user.groups.join()
-        }
-    };
     try {
         await caseworkServiceClient.put(`/case/${caseId}/stage/${stageId}/user`, {
             userUUID: user.uuid,
-        }, headers);
+        }, { headers: User.createHeaders(user) });
     } catch (e) {
         logger.warn(e);
     } finally {
