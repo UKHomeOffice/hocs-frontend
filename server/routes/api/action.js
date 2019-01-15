3696a44c3e454f0650baa600f4bc5fb488faa6ad
Lucas Cairns <lucas.cairns@digital.homeoffice.gov.uk>
2019-01-15 11:26:01 +0000

    Remove permission middleware from forms (#173)

--- server/routes/api/action.js
+++ server/routes/api/action.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { getFormForAction } = require('../../services/form');
-const authMiddleware = require('../../middleware/auth');
 const { fileMiddleware } = require('../../middleware/file');
 const { processMiddleware } = require('../../middleware/process');
 const { validationMiddleware } = require('../../middleware/validation');
@@ -8,7 +7,6 @@ const { apiActionResponseMiddleware } = require('../../middleware/action');
 
 router.post(['/:workflow/:context/:action', '/:workflow/:action'],
     getFormForAction,
-    authMiddleware.protectAction(),
     fileMiddleware.any(),
     processMiddleware,
     validationMiddleware,
