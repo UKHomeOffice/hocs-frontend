d924b1a5c3035b31944662b88fc55893b3003da5
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-08 00:40:27 +0100

    repoint URLs to new casework URLS

--- server/lists/adapters/case-types.js
+++ server/lists/adapters/case-types.js
@@ -3,8 +3,8 @@ const { Choice } = require('../../services/forms/component-builder');
 const byLabel = (a, b) => a.label.localeCompare(b.label);
 
 module.exports = async (data, { logger }) => {
-    logger.debug('REQUEST_CASE_TYPES', { caseTypes: data.caseTypes.length });
-    return data.caseTypes
+    logger.debug('REQUEST_CASE_TYPES', { caseTypes: data.length });
+    return data
         .sort(byLabel)
         .map(({ label, value }) => Choice(label, value));
 };
