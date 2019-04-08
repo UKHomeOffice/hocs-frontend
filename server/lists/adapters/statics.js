ab8525cde6b36f919ac7213920fe208921a37346
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-08 10:05:46 +0100

    Nm remove dtos (#267)
    * return default if the result is null so we don't do null.find
    
    * repoint URLs to new casework URLS

--- server/lists/adapters/statics.js
+++ server/lists/adapters/statics.js
@@ -1,21 +1,21 @@
 const teamsAdapter = async (data, { logger }) => {
-    logger.debug('REQUEST_MINISTERS', { teams: data.length });
+    logger.debug('REQUEST_TEAMS', { teams: data.length });
     return data.map(({ type, displayName }) => ({ key: type, value: displayName }));
 };
 
 const usersAdapter = async (data, { logger }) => {
-    logger.debug('REQUEST_MINISTERS', { users: data.length });
+    logger.debug('REQUEST_USERS', { users: data.length });
     return data.map(({ id, username }) => ({ key: id, value: username }));
 };
 
 const caseTypesAdapter = async (data, { logger }) => {
-    logger.debug('REQUEST_MINISTERS', { caseTypes: data.caseTypes.length });
-    return data.caseTypes.map(({ label, value }) => ({ key: value, value: label }));
+    logger.debug('REQUEST_CASETYPES', { caseTypes: data.length });
+    return data.map(({ label, value }) => ({ key: value, value: label }));
 };
 
 const stageTypesAdapter = async (data, { logger }) => {
-    logger.debug('REQUEST_MINISTERS', { stageTypes: data.stageTypes.length });
-    return data.stageTypes.map(({ label, value }) => ({ key: value, value: label }));
+    logger.debug('REQUEST_STAGETYPE', { stageTypes: data.length });
+    return data.map(({ label, value }) => ({ key: value, value: label }));
 };
 
 module.exports = {
