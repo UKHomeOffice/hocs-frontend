112318ad19d608ba247810192ae0ea5034880f4c
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-15 13:57:29 +0100

    repoint URLs to new casework URLS

--- server/services/forms/schemas/dashboard-search.js
+++ server/services/forms/schemas/dashboard-search.js
@@ -12,6 +12,6 @@ module.exports = (options = {}) => {
                 .withValidator('required', 'Case reference is required')
                 .build()
         )
-        .withPrimaryActionLabel('Go')
+        .withNoPrimaryAction()
         .withSubmissionUrl(submissionUrl);
 };
