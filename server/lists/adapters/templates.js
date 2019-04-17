b71ecc7a966fbb9c86f77d5c7880130878d0599b
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-17 14:27:02 +0100

    repoint URLs to new casework URLS

--- server/lists/adapters/templates.js
+++ server/lists/adapters/templates.js
@@ -1,1 +1,7 @@
-module.exports = async ({ displayName, uuid }) => ([{ label: displayName, value: uuid }]);
+module.exports = async (data) => {
+    return data
+        .map(({ displayName, uuid }) => ({
+            label: displayName,
+            value: uuid
+        }))
+};
