a01eb2d1fdf840a99deba2ac250b5c86fe54f15a
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-23 14:00:06 +0100

    improve panel link UI

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,11 @@
 class user {
     constructor({ id, email, username, roles, groups, uuid }) {
-        this.id = id;
+        this.id = '968672ca-9fc1-491c-b2b9-d13c58b12d7a';
         this.email = email;
         this.username = username;
         this.roles = roles ? roles.split(',').map(p => p.trim()) : [];
         this.groups = groups ? groups.split(',').map(g => g.trim()) : [];
-        this.uuid = uuid;
+        this.uuid = '968672ca-9fc1-491c-b2b9-d13c58b12d7a';
     }
 
     static hasRole(user, role) {
@@ -18,7 +18,7 @@ class user {
 
     static createHeaders(user) {
         return {
-            'X-Auth-UserId': user.uuid,
+            'X-Auth-UserId': '968672ca-9fc1-491c-b2b9-d13c58b12d7a',
             'X-Auth-Roles': user.roles.join(),
             'X-Auth-Groups': user.groups.join()
         };
