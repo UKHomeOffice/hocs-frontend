fc17c884681d7049570fba585031a236b8e37bb2
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-04-24 09:48:59 +0100

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,11 @@
 class user {
     constructor({ id, email, username, roles, groups, uuid }) {
-        this.id = '968672ca-9fc1-491c-b2b9-d13c58b12d7a';
+        this.id = id;
         this.email = email;
         this.username = username;
         this.roles = roles ? roles.split(',').map(p => p.trim()) : [];
         this.groups = groups ? groups.split(',').map(g => g.trim()) : [];
-        this.uuid = '968672ca-9fc1-491c-b2b9-d13c58b12d7a';
+        this.uuid = uuid;
     }
 
     static hasRole(user, role) {
@@ -18,7 +18,7 @@ class user {
 
     static createHeaders(user) {
         return {
-            'X-Auth-UserId': '968672ca-9fc1-491c-b2b9-d13c58b12d7a',
+            'X-Auth-UserId': user.uuid,
             'X-Auth-Roles': user.roles.join(),
             'X-Auth-Groups': user.groups.join()
         };
