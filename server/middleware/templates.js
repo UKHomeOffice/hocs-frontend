ce135ffe8b6005967b0c19faa963968d0d3d8d33
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2019-05-15 16:34:58 +0100

    added headers to template request

--- server/middleware/templates.js
+++ server/middleware/templates.js
@@ -1,13 +1,18 @@
 const { templatesService } = require('../clients');
 const getLogger = require('../libs/logger');
 const { DocumentError } = require('../models/error');
+const User = require('../models/user');
 
 async function getTemplate(req, res, next) {
     const logger = getLogger(req.requestId);
     const { caseId } = req.params;
+    const { user } = req;
+    let headers = {
+        headers: User.createHeaders(user)
+    };
     logger.info('REQUEST_TEMPLATE', { ...req.params });
     try {
-        const response = await templatesService.get(`/template/${caseId}`, { responseType: 'stream' });
+        const response = await templatesService.get(`/template/${caseId}`, headers, { responseType: 'stream' });
         res.setHeader('Cache-Control', 'max-age=86400');
         res.setHeader('Content-Disposition', response.headers['content-disposition']);
         response.data.on('finish', () => logger.debug('REQUEST_TEMPLATE_SUCCESS', { ...req.params }));
