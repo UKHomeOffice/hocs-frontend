6d5238ec92adb12ac3e53a9fe1bab3518fbf206f
simonmitchell <simon.mitchell@digital.homeoffice.gov.uk>
2019-05-15 21:38:20 +0100

    updated templates header map

--- server/middleware/templates.js
+++ server/middleware/templates.js
@@ -12,7 +12,7 @@ async function getTemplate(req, res, next) {
     };
     logger.info('REQUEST_TEMPLATE', { ...req.params });
     try {
-        const response = await templatesService.get(`/template/${caseId}`, { responseType: 'stream' }, headers);
+        const response = await templatesService.get(`/template/${caseId}`, { headers, responseType: 'stream' } );
         res.setHeader('Cache-Control', 'max-age=86400');
         res.setHeader('Content-Disposition', response.headers['content-disposition']);
         response.data.on('finish', () => logger.debug('REQUEST_TEMPLATE_SUCCESS', { ...req.params }));
