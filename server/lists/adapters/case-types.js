cfc605826a90c39e96387154614b820578ac1fa6
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2019-06-03 11:06:51 +0100

    Update case-types.js

--- server/lists/adapters/case-types.js
+++ server/lists/adapters/case-types.js
@@ -3,7 +3,7 @@ const { Choice } = require('../../services/forms/component-builder');
 const byLabel = (a, b) => a.label.localeCompare(b.label);
 
 module.exports = async (data, { logger }) => {
-    logger.debug('REQUEST_CASE_TYPES', { caseTypes: data.length });
+    logger.debug('REQUEST_CASE_TYPES', { caseTypes: data.caseTypeslength });
     return data
         .sort(byLabel)
         .map(({ label, value }) => Choice(label, value));
