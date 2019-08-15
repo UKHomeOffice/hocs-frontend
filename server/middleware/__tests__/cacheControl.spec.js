ee0555e56a419e68b11689c97df50d4136534a3a
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-08-15 11:39:00 +0100

    HOCS-1011-no-cache added additional directives

--- server/middleware/__tests__/cacheControl.spec.js
+++ server/middleware/__tests__/cacheControl.spec.js
@@ -15,7 +15,7 @@ describe('Cache control middleware', () => {
 
     it('should call the setHeader method on the response', async () => {
         await setCacheControl(req, res, next);
-        expect(setHeader).toHaveBeenCalledWith('Cache-Control', 'no-cache');
+        expect(setHeader).toHaveBeenCalledWith('Cache-Control', 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0, max-age=0, s-maxage=0');
     });
 
     it('should call the next handler', async () => {
--- server/middleware/cacheControl.js
+++ server/middleware/cacheControl.js
@@ -1,5 +1,5 @@
 async function setCacheControl(req, res, next) {
-    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0, max-age=0, s-maxage=0');
     next();
 }
 
