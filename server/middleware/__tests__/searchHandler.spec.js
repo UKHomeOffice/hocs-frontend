da62c645cf755f7457ddcf5875d75cede4489244
hubert.raich <hubert.raich@bjss.com>
2019-10-25 17:14:30 +0100

    HOCS-1076-Search for a compensation case

--- server/middleware/__tests__/searchHandler.spec.js
+++ server/middleware/__tests__/searchHandler.spec.js
@@ -13,96 +13,92 @@ const mockUser = { username: 'TEST_USER', uuid: 'TEST', roles: [], groups: [] };
 const next = jest.fn();
 
 describe('handleSearch', () => {
-
-    describe('Required validator', () => {
-
-        beforeEach(() => {
-            next.mockReset();
-            req = {
-                form: {
-                    data: {
-                        'caseTypes': 'CT1',
-                        'dateReceivedTo': '20-10-2019',
-                        'dateReceivedFrom': '10-10-2019',
-                        'correspondent': 'Bob',
-                        'topic': 'Test Topic',
-                        'signOffMinister': 'Min123',
-                        'claimantName': 'test Name',
-                        'claimantDOB': '12-11-1967',
-                        'niNumber': 'SJ0000000',
-                        'caseStatus': ['active']
+    beforeEach(() => {
+        next.mockReset();
+        req = {
+            form: {
+                data: {
+                    'caseTypes': 'CT1',
+                    'dateReceivedTo': '20-10-2019',
+                    'dateReceivedFrom': '10-10-2019',
+                    'correspondent': 'Bob',
+                    'topic': 'Test Topic',
+                    'signOffMinister': 'Min123',
+                    'claimantName': 'test Name',
+                    'claimantDOB': '12-11-1967',
+                    'niNumber': 'SJ0000000',
+                    'caseStatus': ['active']
+                }
+            },
+            requestId: 'reqid',
+            user: mockUser,
+            listService: {
+                getFromStaticList: jest.fn(async (listId, key) => {
+                    if (listId === 'S_TEAMS' && key === 'T1') {
+                        return Promise.resolve('TEAM1');
                     }
-                },
-                requestId: 'reqid',
-                user: mockUser,
-                listService: {
-                    getFromStaticList: jest.fn(async (listId, key) => {
-                        if (listId === 'S_TEAMS' && key === 'T1') {
-                            return Promise.resolve('TEAM1');
-                        }
-                        if (listId === 'S_CASETYPES' && key === 'CT1') {
-                            return Promise.resolve('CaseType1');
-                        }
-                        if (listId === 'S_STAGETYPES' && key === 'ST1') {
-                            return Promise.resolve('StageType1');
-                        }
-                        return Promise.reject();
-                    })
-                }
-            };
-            res = {
-                locals: {}
-            };
-            caseworkService.post.mockImplementation(() => Promise.resolve({
-                data: {
-                    stages: [
-                        { caseReference: 'ref2', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' },
-                        { caseReference: 'ref1', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' },
-                        { caseReference: 'ref3', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' }
-                    ]
-                }
-            }));
-        });
+                    if (listId === 'S_CASETYPES' && key === 'CT1') {
+                        return Promise.resolve('CaseType1');
+                    }
+                    if (listId === 'S_STAGETYPES' && key === 'ST1') {
+                        return Promise.resolve('StageType1');
+                    }
+                    return Promise.reject();
+                })
+            }
+        };
+        res = {
+            locals: {}
+        };
+        caseworkService.post.mockImplementation(() => Promise.resolve({
+            data: {
+                stages: [
+                    { caseReference: 'ref2', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' },
+                    { caseReference: 'ref1', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' },
+                    { caseReference: 'ref3', teamUUID: 'T1', caseType: 'CT1', stageType: 'ST1' }
+                ]
+            }
+        }));
+    });
 
-        it('should create an ordered workstack object on res.locals', async () => {
-            await handleSearch(req, res, next);
+    it('should create an ordered workstack object on res.locals', async () => {
+        await handleSearch(req, res, next);
 
-            const expectedRequest = {
-                caseType: 'CT1',
-                dateReceived: {
-                    to: '20-10-2019',
-                    from: '10-10-2019'
-                },
-                correspondentName: 'Bob',
-                topic: 'Test Topic',
-                data: {
-                    POTeamUUID: 'Min123',
-                    FullName: 'test Name',
-                    DateOfBirth: '12-11-1967',
-                    NI: 'SJ0000000'
-                },
-                activeOnly: true
-            };
+        const expectedRequest = {
+            caseType: 'CT1',
+            dateReceived: {
+                to: '20-10-2019',
+                from: '10-10-2019'
+            },
+            correspondentName: 'Bob',
+            topic: 'Test Topic',
+            data: {
+                POTeamUUID: 'Min123',
+                FullName: 'test Name',
+                DateOfBirth: '12-11-1967',
+                NI: 'SJ0000000'
+            },
+            activeOnly: true
+        };
 
-            const expecteHeaders = {
-                headers: { 'X-Auth-Groups': '', 'X-Auth-Roles': '', 'X-Auth-UserId': 'TEST' }
-            };
+        const expecteHeaders = {
+            headers: { 'X-Auth-Groups': '', 'X-Auth-Roles': '', 'X-Auth-UserId': 'TEST' }
+        };
 
-            expect(res.locals.workstack).toBeDefined();
-            expect(res.locals.workstack.items.length).toEqual(3);
-            expect(res.locals.workstack.items[0].caseReference).toEqual('ref1');
-            expect(res.locals.workstack.items[1].caseReference).toEqual('ref2');
-            expect(res.locals.workstack.items[2].caseReference).toEqual('ref3');
-            expect(res.locals.workstack.items[2].teamUUID).toEqual('T1');
-            expect(res.locals.workstack.items[2].assignedTeamDisplay).toEqual('TEAM1');
-            expect(res.locals.workstack.items[2].caseType).toEqual('CT1');
-            expect(res.locals.workstack.items[2].caseTypeDisplayFull).toEqual('CaseType1');
-            expect(res.locals.workstack.items[2].stageType).toEqual('ST1');
-            expect(res.locals.workstack.items[2].stageTypeDisplay).toEqual('StageType1');
+        expect(res.locals.workstack).toBeDefined();
+        expect(res.locals.workstack.items.length).toEqual(3);
+        expect(res.locals.workstack.items[0].caseReference).toEqual('ref1');
+        expect(res.locals.workstack.items[1].caseReference).toEqual('ref2');
+        expect(res.locals.workstack.items[2].caseReference).toEqual('ref3');
+        expect(res.locals.workstack.items[2].teamUUID).toEqual('T1');
+        expect(res.locals.workstack.items[2].assignedTeamDisplay).toEqual('TEAM1');
+        expect(res.locals.workstack.items[2].caseType).toEqual('CT1');
+        expect(res.locals.workstack.items[2].caseTypeDisplayFull).toEqual('CaseType1');
+        expect(res.locals.workstack.items[2].stageType).toEqual('ST1');
+        expect(res.locals.workstack.items[2].stageTypeDisplay).toEqual('StageType1');
 
-            expect(next).toHaveBeenCalled();
-            expect(caseworkService.post).toHaveBeenCalled();
-            expect(caseworkService.post).toHaveBeenCalledWith('/search', expectedRequest, expecteHeaders);
-        });
+        expect(next).toHaveBeenCalled();
+        expect(caseworkService.post).toHaveBeenCalled();
+        expect(caseworkService.post).toHaveBeenCalledWith('/search', expectedRequest, expecteHeaders);
     });
 });
