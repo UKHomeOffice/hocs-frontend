3789728b66720c401456bcf374676e35aa24e256
raysonviswas <rayson.viswas@live.co.uk>
2019-10-29 13:20:58 +0000

    scrollIntoView is now a function

--- src/shared/common/forms/__tests__/error-summary.spec.js
+++ src/shared/common/forms/__tests__/error-summary.spec.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ErrorSummary from '../error-summary.jsx';
 import { MemoryRouter } from 'react-router-dom';
 
+// eslint-disable-next-line no-console
+const refSpy = jest.spyOn(React, 'createRef').mockImplementation(() => ( { current: { scrollIntoView: () => 'scrolling' } }));
+
 const errors = {
     field1: 'Error 1',
     field2: 'Error 2',
@@ -33,6 +36,11 @@ describe('Form text component', () => {
                 </MemoryRouter >
             )
         ).toMatchSnapshot();
+    });
+
+    it('expect createRef to be called', () => {
+        render(<ErrorSummary description="Displaying a list of the errors on the page" />);
+        expect(refSpy).toBeCalled();
     });
 });
 
