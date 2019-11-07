c2760ee95a4915b1982c164b33273895f2092ea5
raysonviswas <rayson.viswas@live.co.uk>
2019-11-07 12:40:16 +0000

    stages nor appear in alphabetical order

--- server/lists/adapters/__tests__/case-view.spec.js
+++ server/lists/adapters/__tests__/case-view.spec.js
@@ -1,47 +1,100 @@
 const caseViewAdapter = require('../case-view');
 
 const mockFromStaticList = jest.fn((list, stageId) => stageId);
+const mockData = {
+    caseReference: 'MIN/1234567/18',
+    schema: {
+        title: 'View Case',
+        fields: {
+            STAGE_NAME2: [
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_field',
+                        label: 'My Field'
+                    },
+                },
+                {
+                    component: 'date',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_date_field',
+                        label: 'My Date Field'
+                    },
+                },
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_empty_field',
+                        label: 'My Empty Field'
+                    },
+                },
+            ],
+            STAGE_NAME: [
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_field',
+                        label: 'My Field'
+                    },
+                },
+                {
+                    component: 'date',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_date_field',
+                        label: 'My Date Field'
+                    },
+                },
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_empty_field',
+                        label: 'My Empty Field'
+                    },
+                },
+            ],
+            STAGE_NAME1: [
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_field',
+                        label: 'My Field'
+                    },
+                },
+                {
+                    component: 'date',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_date_field',
+                        label: 'My Date Field'
+                    },
+                },
+                {
+                    component: 'text',
+                    validation: ['required'],
+                    props: {
+                        name: 'my_empty_field',
+                        label: 'My Empty Field'
+                    },
+                },
+            ]
+
+        }
+    },
+    data: {
+        my_field: 'Some Value',
+        my_date_field: '2020-01-19'
+    }
+};
 
 describe('Case-view Adapter', () => {
-    it('should build a schema from the provided template', async () => {
-        const mockData = {
-            caseReference: 'MIN/1234567/18',
-            schema: {
-                title: 'View Case',
-                fields: {
-                    STAGE_NAME: [
-                        {
-                            component: 'text',
-                            validation: ['required'],
-                            props: {
-                                name: 'my_field',
-                                label: 'My Field'
-                            },
-                        },
-                        {
-                            component: 'date',
-                            validation: ['required'],
-                            props: {
-                                name: 'my_date_field',
-                                label: 'My Date Field'
-                            },
-                        },
-                        {
-                            component: 'text',
-                            validation: ['required'],
-                            props: {
-                                name: 'my_empty_field',
-                                label: 'My Empty Field'
-                            },
-                        },
-                    ]
-                }
-            },
-            data: {
-                my_field: 'Some Value',
-                my_date_field: '2020-01-19'
-            }
-        };
+    it('should build a schema from the provided template and have sections in alphabetical order', async () => {
 
         const result = await caseViewAdapter(mockData, { fromStaticList: mockFromStaticList });
 
