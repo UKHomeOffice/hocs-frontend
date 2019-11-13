766c80db1367b383ce1a20e8c8ced3f25b85665a
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-13 18:34:45 +0000

    WCS-HOCS-1239 updated the tests

--- src/shared/common/components/__tests__/document-list.spec.jsx
+++ src/shared/common/components/__tests__/document-list.spec.jsx
@@ -4,9 +4,9 @@ import DocumentList from '../document-list.jsx';
 describe('Document list component', () => {
 
     const documentList = [
-        { label: 'TEST_DOCUMENT_1', value: 'MOCK_DOC_ID_1', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_1', file_link: 'MOCK_ORIG_LINK_1', status: 'UPLOADED' },
-        { label: 'TEST_DOCUMENT_2', value: 'MOCK_DOC_ID_2', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_2', file_link: 'MOCK_ORIG_LINK_2', status: 'UPLOADED' },
-        { label: 'TEST_DOCUMENT_3', value: 'MOCK_DOC_ID_3', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_3', file_link: 'MOCK_ORIG_LINK_3', status: 'UPLOADED' },
+        { label: 'TEST_DOCUMENT_1', value: 'MOCK_DOC_ID_1', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_1', file_link: 'MOCK_ORIG_LINK_1', status: 'UPLOADED', tags: ['Matrix'] },
+        { label: 'TEST_DOCUMENT_2', value: 'MOCK_DOC_ID_2', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_2', file_link: 'MOCK_ORIG_LINK_2', status: 'UPLOADED', tags: ['Matrix'] },
+        { label: 'TEST_DOCUMENT_3', value: 'MOCK_DOC_ID_3', type: 'ORIGINAL', pdf_link: 'MOCK_S3_LINK_3', file_link: 'MOCK_ORIG_LINK_3', status: 'UPLOADED', tags: ['Matrix'] },
     ];
 
     it('should render with default props', () => {
