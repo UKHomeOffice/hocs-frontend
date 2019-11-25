b27645855a38da4481903f0589e748211c2b9f97
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-25 14:29:15 +0000

    WCS-HOCS-1325 swapped a setstate for a dispatch and changed back the local pg db path

--- src/shared/pages/form-enabled.jsx
+++ src/shared/pages/form-enabled.jsx
@@ -93,7 +93,7 @@ function withForm(Page) {
             e.preventDefault();
             const { dispatch, track, history, match: { url } } = this.props;
             const { form_schema, form_data } = this.state;
-            this.setState({ form_errors: undefined });
+            dispatch(updateFormErrors(undefined));
             // TODO: Remove
             /* eslint-disable-next-line no-undef */
             const formData = new FormData();
