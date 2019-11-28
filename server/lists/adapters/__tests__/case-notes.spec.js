2ebce460e66e8ace032943286c234e35950ffb0f
Lewis Muirhead <lewis.muirhead@bjss.com>
2019-11-28 13:45:29 +0000

    WCS-HOCS-1249 updated the test and snapshot

--- server/lists/adapters/__tests__/case-notes.spec.js
+++ server/lists/adapters/__tests__/case-notes.spec.js
@@ -29,14 +29,17 @@ describe('Case Notes Adapter', () => {
             { eventTime: '2019-01-01', type: 'MANUAL', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
             { eventTime: '2019-01-03', type: 'STAGE_ALLOCATED_TO_USER', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
             { eventTime: '2019-01-05', type: 'STAGE_ALLOCATED_TO_TEAM', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
+            { eventTime: '2019-01-05', type: 'STAGE_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
+            { eventTime: '2019-01-05', type: 'STAGE_COMPLETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
+            { eventTime: '2019-01-05', type: 'STAGE_RECREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
             { eventTime: '2019-01-07', type: 'CORRESPONDENT_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
             { eventTime: '2019-01-09', type: 'CORRESPONDENT_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
-            { eventTime: '2019-01-10', type: 'CASE_TOPIC_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
-            { eventTime: '2019-01-11', type: 'CASE_TOPIC_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
+            { eventTime: '2019-01-10', type: 'CASE_TOPIC_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, topicName: '__topic__' } },
+            { eventTime: '2019-01-11', type: 'CASE_TOPIC_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, topicName: '__topic__' } },
             { eventTime: '2019-01-08', type: 'CASE_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 } },
-            { eventTime: '2019-01-06', type: 'CASE_UPDATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, document: 'test document', topic: 'test topic', correspondent: 'test correspondent' } },
-            { eventTime: '2019-01-04', type: 'DOCUMENT_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, document: 'test document', topic: 'test topic', correspondent: 'test correspondent' } },
-            { eventTime: '2019-01-02', type: 'DOCUMENT_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, document: 'test document', topic: 'test topic', correspondent: 'test correspondent' } },
+            { eventTime: '2019-01-06', type: 'CASE_UPDATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, document: 'test document', topic: 'test topic', fullname: 'test correspondent' } },
+            { eventTime: '2019-01-04', type: 'DOCUMENT_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, documentTitle: 'test document', topic: 'test topic', fullname: 'test correspondent' } },
+            { eventTime: '2019-01-02', type: 'DOCUMENT_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, documentTitle: 'test document', topic: 'test topic', fullname: 'test correspondent' } },
             { eventTime: '2019-01-01', type: 'TEST_UNKNOWN_TYPE', userName: 'User A' }
         ];
 
