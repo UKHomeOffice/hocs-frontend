f35ae1882645dcfda28458dabc8b0a0427326ce6
Lewis Muirhead <lewis.muirhead@bjss.com>
2020-01-14 16:02:25 +0000

    WCS-HOCS-1411 WIP timeout options

--- src/shared/layouts/session-timer.jsx
+++ src/shared/layouts/session-timer.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import { Context } from '../contexts/application.jsx';
 import { Helmet } from 'react-helmet-async';
 
+const countDown = 60000;
+const defaultTargetDate = new Date().getTime() + countDown;
+
 const SessionTimer = () => {
     const { layout: { header: { service } } } = React.useContext(Context);
-    const [sessionTimeRemaining, setSessionTimeRemaining] = React.useState(60);
+    const [targetDate,] = React.useState(new Date(defaultTargetDate));
+    const [remainingSeconds, setRemainingSeconds] = React.useState(countDown / 1000);
+    let requestRef = React.useRef();
+
+    let localRequestAnimationFrame;
+    // eslint-disable-next-line no-undef
+    if (typeof requestAnimationFrame === 'undefined' || !requestAnimationFrame) {
+        localRequestAnimationFrame = setImmediate;
+    } else {
+        // eslint-disable-next-line no-undef
+        localRequestAnimationFrame = requestAnimationFrame;
+    }
+
+    // eslint-disable-next-line no-undef
+    // let localCancelAnimationFrame = typeof cancelAnimationFrame === 'undefined' ? clearImmediate : cancelAnimationFrame;
+
+    const countItDown = () => localRequestAnimationFrame(() => {
+        const diff = Math.floor((targetDate - new Date().getTime()) / 1000);
+        setRemainingSeconds(diff);
+        console.log(diff);
+        if (diff > 0) {
+            countItDown();
+        }
+    });
 
     React.useEffect(() => {
-        const interval = setInterval(() => {
-            setSessionTimeRemaining(sessionTimeRemaining - 1);
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [sessionTimeRemaining]);
+        countItDown();
+        // requestRef = localRequestAnimationFrame(countItDown);
+        // return () => localCancelAnimationFrame(requestRef.current);
+    }, [targetDate]);
 
-    return sessionTimeRemaining < 55 && <Helmet titleTemplate={`${sessionTimeRemaining}s remaining %s`}>
+    countItDown();
+    return remainingSeconds < 55 && <Helmet defer={false} titleTemplate={`${remainingSeconds}s remaining %s`}>
         <title>{service.toString()}</title>
     </Helmet>;
 };
