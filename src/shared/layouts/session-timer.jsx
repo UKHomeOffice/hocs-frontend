058b99bea324c29f07d5fd2529234f59a37834bb
Lewis Muirhead <lewis.muirhead@bjss.com>
2020-01-16 14:35:11 +0000

    WCS-HOCS-1411 add session timer modal

--- src/shared/layouts/session-timer.jsx
+++ src/shared/layouts/session-timer.jsx
@@ -1,41 +1,65 @@
 import React from 'react';
-import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
+import Modal from 'react-modal';
 import { Context } from '../contexts/application.jsx';
 
 const countDown = 60000;
 const getTargetDate = () => new Date(new Date().getTime() + countDown);
+const getModalTitle = remainingSeconds => remainingSeconds > 0 ? `Your session will expire in ${remainingSeconds} seconds` : 'Your session has expired';
+const getModalMessage = remainingSeconds => remainingSeconds > 0 ? 'We won\'t be able to save what you have done and you\'ll lose your progress. \n Click Continue to extend your session.' : 'You\'ll need to login again.';
+const getButtonText = remainingSeconds => remainingSeconds > 0 ? 'Continue' : 'Return to login';
 
 const SessionTimer = () => {
     const { layout: { header: { service } } } = React.useContext(Context);
-    const [targetDate, setTargetDate] = React.useState(getTargetDate());
+    const [targetDate,] = React.useState(getTargetDate());
     const [remainingSeconds, setRemainingSeconds] = React.useState(countDown / 1000);
-    const [showAsterisk, setShowAsterisk] = React.useState(true);
 
     React.useEffect(() => {
-        // Add a request interceptor
-        axios.interceptors.response.use((response) => {
-            setTargetDate(getTargetDate());
-            console.log(response);
-            return response;
-        }, (error) => {
-            console.log(error);
-            return error;
-        });
+        Modal.setAppElement('#app');
     }, []);
 
     React.useEffect(() => {
         const interval = setInterval(() => {
             const diff = Math.floor((targetDate - new Date().getTime()) / 1000);
             setRemainingSeconds(diff);
-            setShowAsterisk(showAsterisk => setShowAsterisk(!showAsterisk));
+            if (diff <= 0) {
+                clearInterval(interval);
+            }
         }, 500);
-        return () => clearInterval(interval);
-    }, [targetDate, showAsterisk]);
+        return () => {
+            clearInterval(interval);
+        };
+    }, [targetDate]);
 
-    return typeof window !== 'undefined' && <Helmet defer={false} titleTemplate={`${remainingSeconds > 55 ? '' : `${showAsterisk ? '*' : remainingSeconds}s remaining `}%s`}>
-        {service && <title>{service.toString()}</title>}
-    </Helmet>;
+    return <>
+        {typeof window !== 'undefined' && <Helmet defer={false} titleTemplate={`${remainingSeconds > 55 || remainingSeconds <= 0 ? '' : `${remainingSeconds}s remaining `}%s`}>
+            {service && <title>{service.toString()}</title>}
+        </Helmet>}
+        <Modal
+            isOpen={remainingSeconds <= 55}
+            shouldCloseOnOverlayClick={false}
+            shouldCloseOnEsc={false}
+            style={{
+                overlay: {
+                    backgroundColor: '#bdbdbdbd',
+                    display: 'flex'
+                },
+                content: {
+                    border: 'none',
+                    borderRadius: '0px',
+                    margin: 'auto',
+                    position: 'relative'
+                }
+            }} >
+            <h2 className="govuk-heading-l">{getModalTitle(remainingSeconds)}</h2>
+            {getModalMessage(remainingSeconds).split('\n').map((p, i) =>
+                <p className="govuk-body" key={i}>{p}</p>
+            )}
+            <button className="govuk-button">
+                {getButtonText(remainingSeconds)}
+            </button>
+        </Modal>
+    </>;
 };
 
 export default SessionTimer;
