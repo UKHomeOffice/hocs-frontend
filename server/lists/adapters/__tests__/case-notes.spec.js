38868c05e6de00fbf8f25b6c7bf0e5235f04d800
Mark Wright <mark.wright@bjss.com>
2020-06-09 09:30:46 +0100

    HOCS-1742 : cases at Draft Escalate can be closed

--- server/lists/adapters/__tests__/case-notes.spec.js
+++ server/lists/adapters/__tests__/case-notes.spec.js
@@ -40,6 +40,7 @@ describe('Case Notes Adapter', () => {
             { eventTime: '2019-01-06 12:00:00', type: 'CASE_UPDATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, document: 'test document', topic: 'test topic', fullname: 'test correspondent' }, timelineItemUUID: '__timelineItemUUID__' },
             { eventTime: '2019-01-04 12:00:00', type: 'DOCUMENT_CREATED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, documentTitle: 'test document', topic: 'test topic', fullname: 'test correspondent' }, timelineItemUUID: '__timelineItemUUID__' },
             { eventTime: '2019-01-02 12:00:00', type: 'DOCUMENT_DELETED', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1, documentTitle: 'test document', topic: 'test topic', fullname: 'test correspondent' }, timelineItemUUID: '__timelineItemUUID__' },
+            { eventTime: '2019-01-05 12:00:00', type: 'CLOSE', userName: 'User A', body: { caseNote: 'A test case note', userUUID: 1, teamUUID: 1, stage: 1 }, timelineItemUUID: '__timelineItemUUID__' },
             { eventTime: '2019-01-01 12:00:00', type: 'TEST_UNKNOWN_TYPE', userName: 'User A', timelineItemUUID: '__timelineItemUUID__' }
         ];
 
--- server/lists/adapters/case-notes.js
+++ server/lists/adapters/case-notes.js
@@ -80,6 +80,10 @@ const typeAdaptors = {
         note,
         title: 'Allocation Note'
     }),
+    CLOSE: ({ note }) => ({
+        note,
+        title: 'Case closure note'
+    }),
     REJECT: ({ note }) => ({
         note,
         title: 'Rejection Note'
