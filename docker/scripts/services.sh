8f81a1ca547474da3b8e762b78b78a987976a524
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2020-10-28 14:19:44 +0000

    HOCS-1990: remove unnecessary ES file
    As a result of the elastic search index being moved to our private repo, we no longer have a need to instantiate with this empty file. General cleanup.

--- docker/scripts/services.sh
+++ docker/scripts/services.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Pulling latest and setting up services"
-curl -X PUT http://localstack:4571/local-case -H "Content-Type: application/json" -d @elastic-search-index.json
 docker-compose up documents audit workflow casework info search templates
