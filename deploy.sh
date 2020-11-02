1f4f6f20dfa2877db8b5d08f12d30b7d2e2c8b81
Paul Rau <52495258+Paulcr-ho@users.noreply.github.com>
2020-11-02 13:59:09 +0000

    Hocs 2097 autoscale replicas (#81)
    * HOCS-2097 Autoscale Replicas
    The minimum instances for a given target environment is set in the deploy script. Using this value to set the default number of minimum instances for the service.
    
    * HOCS-2097 Autoscale Replicas
    Maximum replicas have been added to the autoscaler.
    The value is set in the deploy script along with the minimum replicas.

--- deploy.sh
+++ deploy.sh
@@ -13,38 +13,39 @@ export DOMAIN=${DOMAIN}
 
 export IP_WHITELIST=${POISE_WHITELIST}
 
+if [[ ${KUBE_NAMESPACE} == *prod ]]
+then
+    export MIN_REPLICAS="2"
+    export MAX_REPLICAS="6"
+else
+    export MIN_REPLICAS="1"
+    export MAX_REPLICAS="2"
+fi
+
 if [[ ${KUBE_NAMESPACE} == "cs-prod" ]] ; then
     echo "deploy ${VERSION} to PROD namespace, using HOCS_FRONTEND_PROD_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_PROD_CS}
-    export REPLICAS="2"
 elif [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
     echo "deploy ${VERSION} to PROD namespace, using HOCS_FRONTEND_PROD_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_PROD_WCS}
-    export REPLICAS="2"
 elif [[ ${KUBE_NAMESPACE} == "cs-qa" ]] ; then
     echo "deploy ${VERSION} to QA namespace, using HOCS_FRONTEND_QA_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_QA_CS}
-    export REPLICAS="1"
 elif [[ ${KUBE_NAMESPACE} == "wcs-qa" ]] ; then
     echo "deploy ${VERSION} to QA namespace, using HOCS_FRONTEND_QA_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_QA_WCS}
-    export REPLICAS="1"
 elif [[ ${KUBE_NAMESPACE} == "cs-demo" ]] ; then
     echo "deploy ${VERSION} to DEMO namespace, HOCS_FRONTEND_DEMO_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEMO_CS}
-    export REPLICAS="1"
 elif [[ ${KUBE_NAMESPACE} == "wcs-demo" ]] ; then
     echo "deploy ${VERSION} to DEMO namespace, HOCS_FRONTEND_DEMO_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEMO_WCS}
-    export REPLICAS="1"
 elif [[ ${KUBE_NAMESPACE} == "cs-dev" ]] ; then
     echo "deploy ${VERSION} to DEV namespace, HOCS_FRONTEND_DEV_CS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEV_CS}
-    export REPLICAS="1"
 elif [[ ${KUBE_NAMESPACE} == "wcs-dev" ]] ; then
     echo "deploy ${VERSION} to DEV namespace, HOCS_FRONTEND_DEV_WCS drone secret"
     export KUBE_TOKEN=${HOCS_FRONTEND_DEV_WCS}
-    export REPLICAS="1"
 else
     echo "Unable to find environment: ${ENVIRONMENT}"
 fi
