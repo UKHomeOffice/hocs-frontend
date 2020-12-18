ffd9e1a99a7c666e559b92f7d24be48ab887c573
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2020-12-18 09:44:52 +0000

    HOCS-2259: change exporting
    When building we currently don't support ES6 exporting of functions. We are currently restricted to using module.exports.

--- server/libs/dateHelpers.js
+++ server/libs/dateHelpers.js
@@ -1,13 +1,15 @@
-export const addDays = (dateString, days) => {
+const addDays = (dateString, days) => {
     const date = new Date(dateString);
     date.setDate(date.getDate() + days);
     return date;
 };
 
 // Returns today's date in format (yyyy-MM-dd)
-export const getUtcDateString = (date) => {
+const getUtcDateString = (date) => {
     if (date && date instanceof Date) {
         return date.toISOString().split('T')[0];
     }
     return undefined;
 };
+
+module.exports = { addDays, getUtcDateString };
