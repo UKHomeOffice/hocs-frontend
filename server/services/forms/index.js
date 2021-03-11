271b0e4d386f5147a3e62230c7f01ce891e948bf
Mark Wright <46746088+MarkWrightBjss@users.noreply.github.com>
2021-03-11 09:20:31 +0000

    HOCS-2581 : Rename "CMS" case type to "COMP" (#600)
    * HOCS-2536 : adding CMS case type
    
    * HOCS-2581 : Rename "CMS" case type to "COMP"

--- server/services/forms/index.js
+++ server/services/forms/index.js
@@ -24,7 +24,7 @@ const formDefinitions = {
                 }
             },
             DOCUMENT: {
-                CMS: {
+                COMP: {
                     builder: formRepository.addDocument,
                     action: CREATE_CASE,
                     next: {
@@ -88,7 +88,7 @@ const formDefinitions = {
                 }
             },
             DOCUMENT: {
-                CMS: {
+                COMP: {
                     builder: formRepository.bulkAddDocument,
                     action: BULK_CREATE_CASE,
                     next: {
