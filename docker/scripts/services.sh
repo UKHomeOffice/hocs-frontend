2e423d71823a3ce829cb93386c147a626e81a27a
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-04-26 16:32:57 +0100

    Hocs 2527 additional config case creator (#617)
    * HOCS-2527 - Add config for case creator.
    
    Also :
    - Fixed postgres error
    - Added a sleep, to aws_cli. Without the sleep, it gets run multiple times.
    - Set images to main rather than latest
    
    * HOCS-2527 - Make variable default consistent

--- docker/scripts/services.sh
+++ docker/scripts/services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Pulling latest and setting up services"
-docker-compose up documents audit workflow casework info search templates
+docker-compose up documents audit workflow casework info search templates case_creator
