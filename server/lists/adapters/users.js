24ea50a2295fa0cacee5eff476674dbd4e0dedcb
agraham19 <35735033+agraham19@users.noreply.github.com>
2021-05-27 11:24:00 +0100

    HOCS-3077 fix frontend user issue (#647)
    Currently the frontend pulls a list of users for each approal team at startup.
    When this list is pulled, there is a comparison operator to check if each user
    is the requesting user. At startup, however, there is no user, as it's not
    triggered by a requested. This crashes the frontend.
    
    * Only check the user matches the pulled user if one exists

--- server/lists/adapters/users.js
+++ server/lists/adapters/users.js
@@ -3,10 +3,11 @@ const byLabel = (a, b) => a.label.toUpperCase().localeCompare(b.label.toUpperCas
 module.exports = async (data, { user, logger }) => {
     logger.debug('REQUEST_USERS', { users: data.length });
     return data
-        .filter(u => u.id !== user.id)
+        .filter(u => user && (u.id !== user.id))
         .map(({ id, firstName, lastName, email }) => ({
             label: `${firstName} ${lastName} (${email})`,
             value: id
         }))
         .sort(byLabel);
+
 };
