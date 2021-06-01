baee4e1bbf6f041754e453c85e5e1ebee85b4563
Mark Wright <46746088+MarkWrightBjss@users.noreply.github.com>
2021-06-01 15:08:16 +0100

    HOCS-3092 : "Add business contribution" link, fix (#650)

--- server/services/forms/index.js
+++ server/services/forms/index.js
@@ -262,7 +262,7 @@ const formDefinitions = {
                 }
             }
         },
-        CCT_CASE_CONTRIB: {
+        CCT_BUS_CONTRIB: {
             COMP: {
                 ADDREQUEST: {
                     builder: formRepository.contributionRequest,
