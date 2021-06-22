8a01eb318ce571d2272ae0581cc02253610c6e64
Robert Smallwood <rob@fewtrees.com>
2021-06-22 14:25:19 +0100

    HOCS-3079 Add explicit equality test for IS_NOTPROD env var

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const workflowAuth = (process.env.WORKFLOW_BASIC_AUTH || 'UNSET:UNSET').split(':');
-const isNotProd = process.env.IS_NOTPROD == 1;
+const isNotProd = process.env.IS_NOTPROD === '1';
 
 const config = {
     applications: {
