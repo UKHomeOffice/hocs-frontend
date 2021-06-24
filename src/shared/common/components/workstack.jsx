03e31e613faf6d1d2ce1fafcbc4c1d52247bcf44
Sam Gardner <sgardner@caci.co.uk>
2021-06-24 17:05:22 +0100

    HOCS-3103 Using new data from somu table rather than case data to show contribution status

--- src/shared/common/components/workstack.jsx
+++ src/shared/common/components/workstack.jsx
@@ -387,13 +387,14 @@ class WorkstackAllocate extends Component {
             case ColumnRenderer.TRUNCATE_TEXT:
                 return <td key={row.uuid + column.dataValueKey} className='govuk-table__cell govuk-table__cell--truncated' title={value}>{value}</td>;
             case ColumnRenderer.CONTRIBUTIONS_WARNING:
-                if (row.data.CaseContributions) {
-                    const dueContribution = JSON.parse(row.data.CaseContributions)
-                        .filter(contribution => contribution.data && !contribution.data.contributionStatus)
-                        .map(contribution => contribution.data.contributionDueDate)
+                if (row.somu && row.somu.caseContributions) {
+                    const dueContribution = row.somu.caseContributions
+                        .filter(contribution => !JSON.parse(contribution).contributionStatus)
+                        .map(contribution => JSON.parse(contribution).contributionDueDate)
                         .sort()
                         .shift();
-                    if (dueContribution && new Date(dueContribution) <= new Date()) {
+                    
+                    if (dueContribution && new Date(dueContribution) < new Date()) {
                         return <td key={row.uuid + column.dataValueKey} className='govuk-table__cell indicator'>
                             {value && <span title={value} className='indicator-red'>
                                 {value}
