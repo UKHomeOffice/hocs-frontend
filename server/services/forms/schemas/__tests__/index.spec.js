9e5bed4540591ad319fdf191fa25d460c8f38529
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2021-08-10 11:15:22 +0100

    HOCS-3449 fixing skipping tests and adding coverage for exgratia requests

--- server/services/forms/schemas/__tests__/index.spec.js
+++ server/services/forms/schemas/__tests__/index.spec.js
@@ -30,8 +30,8 @@ jest.mock('../../../../services/list/service', () => ({
 }));
 
 
-describe('Form schema definitions', async () => {
-    await Object.entries(formRepository).map(async ([label, form]) => {
+describe('Form schema definitions', () => {
+    Object.entries(formRepository).map(async ([label, form]) => {
         it(`${label} should be a valid schema definition `, async () => {
             const result = await form.call(this, { user: { id: 1234, roles: [], groups: [] } });
             expect(result).toBeDefined();
@@ -44,12 +44,19 @@ describe('Form schema definitions', async () => {
 
 });
 
-describe('Contribution Request', async () => {
+describe('Contribution Request', () => {
 
     const mpamContributionsRequest = {
         showBusinessUnits: true,
         primaryChoiceLabel: 'Business Area',
         primaryChoiceList: 'MPAM_CONTRIBUTION_BUSINESS_AREAS'
+    };
+
+    const exgratiaContritbutionRequest = {
+        showBusinessUnits: false,
+        primaryChoiceLabel: 'Business Area Representative',
+        primaryChoiceList: 'EX_GRATIA_BUS_REPS',
+        showContributionAmount: true
     };
 
     it('should generate form with action as add', async () => {
@@ -59,6 +66,12 @@ describe('Contribution Request', async () => {
         expect(result.schema.title).toBeDefined();
         expect(result.schema.title).toEqual('Add Contribution Request');
         expect(result.schema.fields).toBeDefined();
+        expect(result.schema.fields.length).toEqual(5);
+        expect(result.schema.fields[0].props.label).toEqual('Business Area');
+        expect(result.schema.fields[1].props.label).toEqual('Business Unit');
+        expect(result.schema.fields[2].props.label).toEqual('Contribution request date');
+        expect(result.schema.fields[3].props.label).toEqual('Contribution due date');
+        expect(result.schema.fields[4].props.label).toEqual('What you are requesting');
         expect(result.schema.defaultActionLabel).toBeDefined();
         expect(result.schema.defaultActionLabel).toEqual('Add');
     });
@@ -70,7 +83,30 @@ describe('Contribution Request', async () => {
         expect(result.schema.title).toBeDefined();
         expect(result.schema.title).toEqual('Edit Contribution Request');
         expect(result.schema.fields).toBeDefined();
+        expect(result.schema.fields.length).toEqual(5);
+        expect(result.schema.fields[0].props.label).toEqual('Business Area');
+        expect(result.schema.fields[1].props.label).toEqual('Business Unit');
+        expect(result.schema.fields[2].props.label).toEqual('Contribution request date');
+        expect(result.schema.fields[3].props.label).toEqual('Contribution due date');
+        expect(result.schema.fields[4].props.label).toEqual('What you are requesting');
         expect(result.schema.defaultActionLabel).toBeDefined();
         expect(result.schema.defaultActionLabel).toEqual('Edit');
+    });
+
+    it('should generate form with optional request amount field', async () => {
+        const result = await formRepository.contributionRequest({ user: { id: 1234, roles: [], groups: [] }, action: 'addRequest', customConfig: exgratiaContritbutionRequest });
+        expect(result).toBeDefined();
+        expect(result.schema).toBeDefined();
+        expect(result.schema.title).toBeDefined();
+        expect(result.schema.title).toEqual('Add Contribution Request');
+        expect(result.schema.fields).toBeDefined();
+        expect(result.schema.fields.length).toEqual(5);
+        expect(result.schema.fields[0].props.label).toEqual('Business Area Representative');
+        expect(result.schema.fields[1].props.label).toEqual('Contribution request date');
+        expect(result.schema.fields[2].props.label).toEqual('Contribution due date');
+        expect(result.schema.fields[3].props.label).toEqual('Contribution Amount (£)');
+        expect(result.schema.fields[4].props.label).toEqual('What you are requesting');
+        expect(result.schema.defaultActionLabel).toBeDefined();
+        expect(result.schema.defaultActionLabel).toEqual('Add');
     });
 });
