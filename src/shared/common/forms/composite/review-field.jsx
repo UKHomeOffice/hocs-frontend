76d46474169f5884a58a81ac51cf2cfaa13535d1
agraham19 <35735033+agraham19@users.noreply.github.com>
2021-10-07 09:20:32 +0100

    HOCS-3759 Capture Outcomes and Exemptions: Check your answers screen is not displayed (#772)
    * Extend review-field to handle checkbox grid component

--- src/shared/common/forms/composite/review-field.jsx
+++ src/shared/common/forms/composite/review-field.jsx
@@ -6,7 +6,7 @@ const findLabelByValue = (choices, value) => {
     return choices.find(choice => choice.value === value).label;
 };
 
-const renderChangeDirectionLink = (child, switchDirection, direction) => {
+function renderChangeDirectionLink(child, switchDirection, direction) {
     return <>
         <a className="govuk-body govuk-link change-link" href="" onClick={(e) => {
             e.preventDefault();
@@ -15,9 +15,9 @@ const renderChangeDirectionLink = (child, switchDirection, direction) => {
             Change <span className={'govuk-visually-hidden'}>{child.props.label}</span>
         </a>
     </>;
-};
+}
 
-const renderChangeEntityLink = (child, page, value, caseType) => {
+function renderChangeEntityLink(child, page, value, caseType) {
     const caseTypeParam = caseType ? caseType + '/' : '';
 
     return <>
@@ -26,37 +26,113 @@ const renderChangeEntityLink = (child, page, value, caseType) => {
             Change <span className={'govuk-visually-hidden'}>{child.props.label}</span>
         </Link>
     </>;
-};
+}
 
 const ReviewField = ({ name, type, value, child, switchDirection, direction, page, caseType }) => {
-    let textValue = value;
-    if (child.component === 'radio' || child.component === 'dropdown' || child.component === 'entity-list' || child.component === 'type-ahead') {
-        textValue = findLabelByValue(child.props.choices, value);
-    } else if (child.component === 'date') {
-        const date = new Date(value);
-        if (isNaN(date) || value == null) {
-            textValue = '';
-        } else {
-            textValue = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
-        }
+    const changeLineBuilder = ChangeAttributeBuilder();
+
+    switch(child.component){
+        case 'radio':
+        case 'dropdown':
+        case 'type-ahead':
+            changeLineBuilder.addValue(findLabelByValue(child.props.choices, value));
+            changeLineBuilder.withChangeLink(renderChangeDirectionLink(child, switchDirection, direction));
+            break;
+
+        case 'entity-list':
+            changeLineBuilder.addValue(findLabelByValue(child.props.choices, value));
+            changeLineBuilder.withChangeLink(renderChangeEntityLink(child, page, value, caseType));
+            break;
+
+        case 'date':
+            // eslint-disable-next-line no-case-declarations
+            const date = new Date(value);
+
+            if (isNaN(date) || value == null) {
+                changeLineBuilder.addValue('');
+
+            } else {
+                changeLineBuilder
+                    .addValue(`${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1)
+                        .toString().padStart(2, '0')}/${date.getFullYear()}`);
+            }
+            changeLineBuilder.withChangeLink(renderChangeDirectionLink(child, switchDirection, direction));
+
+            break;
+
+        case 'checkbox-grid':
+            value.split(',').forEach(checkboxValue => {
+                changeLineBuilder.addValue(findLabelByValue(child.props.choices, checkboxValue));
+            });
+
+            changeLineBuilder.withChangeLink(renderChangeDirectionLink(child, switchDirection, direction));
+
+            break;
+        default:
+            changeLineBuilder.addValue(value);
+            changeLineBuilder.withChangeLink(renderChangeDirectionLink(child, switchDirection, direction));
     }
 
     return (
-        <div className='govuk-grid-row grey-border-bottom'>
-            <div className='govuk-grid-column-one-half'><span className={'govuk-label govuk-label--s'}>{child.props.label}</span></div>
-            <div className={'govuk-grid-column-one-third govuk-body review-field-column'}
-                id={name}
-                type={type}
-                name={name}>
-                {textValue}
-            </div>
-            <div className={'grid-column-one-sixth-right-align govuk-body review-field-column'}>
-                {child.component === 'entity-list' && renderChangeEntityLink(child, page, value, caseType)}
-                {child.component !== 'entity-list' && renderChangeDirectionLink(child, switchDirection, direction)}
-            </div>
-        </div>
+        changeLineBuilder
+            .withName(name)
+            .withType(type)
+            .withLabel(child.props.label)
+            .build()
     );
 };
+
+function ChangeAttributeBuilder() {
+
+    return {
+        values: [],
+
+        withName: function(name) {
+            this.name = name;
+            return this;
+        },
+        withLabel: function(label) {
+            this.label = label;
+            return this;
+        },
+        withType: function(type) {
+            this.type = type;
+            return this;
+        },
+        addValue: function(value) {
+            this.values.push(value);
+            return this;
+        },
+        withChangeLink: function(changeLink) {
+            this.changeLink = changeLink;
+            return this;
+        },
+        build: function build() {
+            return <div>
+                {this.values.map((value, index) => {
+                    return <div key={`${index}`} className='govuk-grid-row review-field-row'>
+                        <div className='govuk-grid-column-one-half'>
+                            <span className={'govuk-label govuk-label--s'}>
+                                {index === 0 && this.label /* only display the label alongside the first value */}
+                            </span>
+                        </div>
+                        <div className={'govuk-grid-column-one-third govuk-body review-field-column'}
+                            id={this.name}
+                            type={this.type}
+                            name={this.name}>
+                            {value}
+                        </div>
+
+                        <div className={'grid-column-one-sixth-right-align govuk-body review-field-column'}>
+                            {this.changeLink}
+                        </div>
+                    </div>;
+                })}
+            </div>;
+        }
+    };
+}
+
 
 ReviewField.propTypes = {
     choices: PropTypes.arrayOf(PropTypes.object),
