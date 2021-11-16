0c83851838be947badac1ffae96281c418fe501b
agraham19 <35735033+agraham19@users.noreply.github.com>
2021-11-16 16:35:07 +0000

    HOCS-4175 Frontend is crashing when a case is loaded (#802)
    * HOCS-4175 Frontend is crashing when a case is loaded
    
    * Add missing summary adaptor
    
    * add newline

---
+++ server/lists/adapters/actions/externalInterestSummaryAdapter.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+module.exports = (externalInterests) => {
+    // External interests are not currently required for display in case summary screen.
+    return null;
+};
