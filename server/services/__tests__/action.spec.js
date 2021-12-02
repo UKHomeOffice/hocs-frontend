65a05cea278810067be389a73bb56c35eeb2fbf9
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2021-12-02 19:42:23 +0000

    HOCS-4103 - PIT Extensions
    * Disallow PIT Extensions after the case deadline

--- server/services/__tests__/action.spec.js
+++ server/services/__tests__/action.spec.js
@@ -1,5 +1,6 @@
 const actionService = require('../action');
 const actionTypes = require('../actions/types');
+const { ActionError } = require('../../models/error');
 
 const mockRequestClient = jest.fn();
 
@@ -106,6 +107,15 @@ jest.mock('../../clients', () => {
             },
             delete: (url, body) => {
                 mockRequestClient(body);
+            },
+            get: (url) => {
+                mockRequestClient(url);
+
+                return {
+                    data: {
+                        caseDeadline: '2021-01-10'
+                    }
+                };
             }
         },
         infoService: {
@@ -461,6 +471,10 @@ describe('Action service', () => {
     });
 
     it('should return a callback url when "APPLY_CASE_DEADLINE_EXTENSION" action succeeds', async () => {
+        const originalNowFunc = Date.now;
+        Date.now = jest.fn(() =>
+            new Date(Date.UTC(2021, 0, 10, 12)).valueOf());
+
         const testForm = {
             schema: {},
             data: {
@@ -495,6 +509,39 @@ describe('Action service', () => {
         expect(mockRequestClient).toHaveBeenCalledWith(expectedBody);
         expect(response).toBeDefined();
         expect(response).toHaveProperty('confirmation');
+
+        Date.now = originalNowFunc();
+    });
+
+    it('should throw an error when "APPLY_CASE_DEADLINE_EXTENSION" applied with lapsed deadline', async () => {
+        const originalNowFunc = Date.now;
+        Date.now = jest.fn(() =>
+            new Date(Date.UTC(2021, 0, 11, 12)).valueOf());
+
+        const testForm = {
+            schema: {},
+            data: {
+                caseTypeActionUuid: '__uuid__',
+                extendBy: 20,
+                extendFrom: 'ANY',
+                note: 'SOME NOTE'
+            },
+            action: actionTypes.APPLY_CASE_DEADLINE_EXTENSION,
+            next: {
+                action: 'CONFIRMATION_SUMMARY'
+            }
+        };
+
+        await expect(actionService.performAction('CASE', {
+            caseId: 1234,
+            stageId: 5678,
+            caseActionType: 'extension',
+            context: null,
+            form: testForm,
+            user: mockUser,
+        })).rejects.toThrow('Failed to perform action');
+
+        Date.now = originalNowFunc;
     });
 
     it('should return a callback when ADD_APPROVAL_REQUEST action succeeds', async () => {
