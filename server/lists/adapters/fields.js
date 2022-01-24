17d382aa88bf0e23efc44298de70fdf0c0019989
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-01-24 16:54:21 +0000

    Removed redundant file

--- server/lists/adapters/fields.js
+++
@@ -1,8 +0,0 @@
-// const fieldsAdapter = async (data, { logger }) => {
-//     logger.debug('REQUEST_FIELDS', { fields: data.length });
-//     return data.map(({ label, value }) => ({ key: value, value: label }));
-// };
-//
-// module.exports = {
-//     fieldsAdapter
-// }
