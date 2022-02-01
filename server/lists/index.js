fe167149adb91eb4f4fe20fea016e248c489f112
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2022-02-01 08:30:21 +0000

    HOCS-4468 Additional Document Types for Treat Official
    * Update Final Response documents list endpoint url

--- server/lists/index.js
+++ server/lists/index.js
@@ -549,7 +549,7 @@ module.exports = {
         },
         CASE_DOCUMENT_LIST_FINAL_RESPONSE: {
             client: 'CASEWORK',
-            endpoint: '/case/document/reference/${caseId}/?type=Final%20response',
+            endpoint: '/case/document/reference/${caseId}/?type=Final%20Response',
             adapter: documentListAdapter
         },
         // todo: this should be removed and updated to use the non tenant specific option above.
