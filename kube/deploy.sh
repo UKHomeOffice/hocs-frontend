392bc9cf7019537b442b3d1578fdb794eba5498f
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2022-02-15 23:11:22 +0000

    HOCS-4580 Specify TLS cert for kd

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -18,6 +18,7 @@ if [[ ${KUBE_NAMESPACE} == *prod ]]; then
     export MAX_REPLICAS="3"
     export ALLOWED_IPS=${POISE_IPS}
     export KUBE_SERVER=https://kube-api-prod.prod.acp.homeoffice.gov.uk
+    export CLUSTER_NAME="acp-prod"
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
     export KUBE_IS_NOTPROD=0
     export SHOW_ERROR_STACKTRACE=0
@@ -38,6 +39,7 @@ else
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
     export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
     export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
+    export CLUSTER_NAME="acp-notprod"
     export ALLOWED_IPS=${POISE_IPS}
 
     export DOMAIN_NAME="${SUBNAMESPACE}.${DOMAIN}-notprod.homeoffice.gov.uk"
@@ -56,6 +58,8 @@ else
     fi
 fi
 
+export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
+
 echo
 echo "Deploying hocs-frontend to ${ENVIRONMENT}"
 echo "Keycloak realm: ${KC_REALM}"
@@ -65,7 +69,7 @@ echo
 
 cd kd
 
-kd --insecure-skip-tls-verify \
+kd  \
    --timeout 10m \
     ${INTERNAL_DOMAIN_NAME:+ -f ingress-internal.yaml} \
     -f ingress-external.yaml \
