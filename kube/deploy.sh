9909558d52fca6f6cfa912ad5a78b7d6bf1930d5
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2022-02-17 23:32:52 +0000

    Hocs 4580 (#879)
    * HOCS-4580 Specify TLS cert for kd
    
    * HOCS-4580 Remove duplicate "is notprod?" check
    
    The new TLS certificate switch exposes whether we're on production or
    not, so this commit refactors the existing "IS_NOTPROD" environment
    variable away and replaces it with a Go template string conditional on
    the cluster name.
    
    Now, if we're on acp-prod that environment variable won't be set at all.
    Otherwise, it will be set to "1".
    
    Co-authored-by: Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -18,10 +18,9 @@ if [[ ${KUBE_NAMESPACE} == *prod ]]; then
     export MAX_REPLICAS="3"
     export ALLOWED_IPS=${POISE_IPS}
     export KUBE_SERVER=https://kube-api-prod.prod.acp.homeoffice.gov.uk
+    export CLUSTER_NAME="acp-prod"
     export UPTIME_PERIOD="Mon-Sun 05:00-23:00 Europe/London"
-    export KUBE_IS_NOTPROD=0
     export SHOW_ERROR_STACKTRACE=0
-    
   if [[ "${KUBE_NAMESPACE}" == "wcs-prod" ]] ; then
       export DOMAIN_NAME="www.wcs.homeoffice.gov.uk"
       export INTERNAL_DOMAIN_NAME=''
@@ -38,11 +37,11 @@ else
     export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
     export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
     export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
+    export CLUSTER_NAME="acp-notprod"
     export ALLOWED_IPS=${POISE_IPS}
 
     export DOMAIN_NAME="${SUBNAMESPACE}.${DOMAIN}-notprod.homeoffice.gov.uk"
     export INTERNAL_DOMAIN_NAME="${SUBNAMESPACE}.internal.${DOMAIN}-notprod.homeoffice.gov.uk"
-    export KUBE_IS_NOTPROD=1
     export SHOW_ERROR_STACKTRACE=1
 
     # but remove the ingress for demo (preprod)
@@ -56,6 +55,8 @@ else
     fi
 fi
 
+export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${CLUSTER_NAME}.crt"
+
 echo
 echo "Deploying hocs-frontend to ${ENVIRONMENT}"
 echo "Keycloak realm: ${KC_REALM}"
@@ -65,7 +66,7 @@ echo
 
 cd kd
 
-kd --insecure-skip-tls-verify \
+kd  \
    --timeout 10m \
     ${INTERNAL_DOMAIN_NAME:+ -f ingress-internal.yaml} \
     -f ingress-external.yaml \
