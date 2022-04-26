189957db879e7b91d5ebc74de0586a17fe1af81b
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-04-26 11:23:51 +0100

    Updated to React 18, updated case note test

--- src/shared/common/components/__tests__/case-note.spec.jsx
+++ src/shared/common/components/__tests__/case-note.spec.jsx
@@ -1,5 +1,49 @@
+// import React from 'react';
+// import CaseNote from '../case-note.jsx';
+//
+// describe('When the component is rendered', () => {
+//     const mockDispatch = jest.fn();
+//     const useContextSpy = jest.spyOn(React, 'useContext').mockImplementation(() => ({
+//         dispatch: mockDispatch,
+//         page: {
+//             params: {
+//                 caseId: '__caseId__'
+//             }
+//         },
+//         track: jest.fn(),
+//         hasRole: jest.fn().mockReturnValue(true)
+//     }));
+//
+//     it('should match the snapshot', () => {
+//         const props = {
+//             author: '__author__',
+//             date: '__date__',
+//             modifiedBy: '__modifiedBy__',
+//             modifiedDate: '__modifiedDate__',
+//             note: '__note__',
+//             timelineItemUUID: '__timelineItemUUID__',
+//             title: '__title__'
+//         };
+//         const wrapper = shallow(<CaseNote {...props} />);
+//
+//         expect(wrapper).toBeDefined();
+//         expect(useContextSpy).toHaveBeenCalled();
+//         expect(wrapper).toMatchSnapshot();
+//     });
+// });
+
+
+
+// import React so you can use JSX (React.createElement) in your test
 import React from 'react';
 import CaseNote from '../case-note.jsx';
+import '@testing-library/jest-dom';
+
+/**
+ * render: lets us render the component as React would
+ * screen: a utility for finding elements the same way the user does
+ */
+import {render, screen} from '@testing-library/react'
 
 describe('When the component is rendered', () => {
     const mockDispatch = jest.fn();
@@ -14,7 +58,7 @@ describe('When the component is rendered', () => {
         hasRole: jest.fn().mockReturnValue(true)
     }));
 
-    it('should match the snapshot', () => {
+    test('should match the snapshot', () => {
         const props = {
             author: '__author__',
             date: '__date__',
@@ -24,10 +68,14 @@ describe('When the component is rendered', () => {
             timelineItemUUID: '__timelineItemUUID__',
             title: '__title__'
         };
-        const wrapper = shallow(<CaseNote {...props} />);
 
+        const wrapper = render(<CaseNote {...props} />);
         expect(wrapper).toBeDefined();
         expect(useContextSpy).toHaveBeenCalled();
-        expect(wrapper).toMatchSnapshot();
+
+        expect(screen.getByText('Edited on __modifiedDate__ - __modifiedBy__')).toBeInTheDocument();
+
+
+        //expect(wrapper).toMatchSnapshot();
     });
 });
