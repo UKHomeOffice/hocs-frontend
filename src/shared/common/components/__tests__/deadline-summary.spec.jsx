410335ea31eaa00cc846543d990be54e5dc2ab0d
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-05-03 08:07:10 +0100

    Updated deadline-summary.spec.jsx to RTL

--- src/shared/common/components/__tests__/deadline-summary.spec.jsx
+++ src/shared/common/components/__tests__/deadline-summary.spec.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import DeadlineSummary from '../deadline-summary.jsx';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 
 describe('Deadline summary component', () => {
 
