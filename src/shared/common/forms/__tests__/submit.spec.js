92fb77ddcc47499aedc224128b0681c3bac9bf10
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-05-03 08:20:19 +0100

    Updated submit.spec.js to RTL

--- src/shared/common/forms/__tests__/submit.spec.js
+++ src/shared/common/forms/__tests__/submit.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Submit from '../submit.jsx';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 
 describe('Form button component', () => {
     it('should render with default props', () => {
@@ -9,17 +11,12 @@ describe('Form button component', () => {
     });
     it('should render disabled when isDisabled is passed', () => {
         expect(
-            render(<Submit isDisabled />)
+            render(<Submit disabled={true} />)
         ).toMatchSnapshot();
     });
     it('should render with correct when label is passed', () => {
         expect(
             render(<Submit label="Submit my form" />)
-        ).toMatchSnapshot();
-    });
-    it('should render with additional styles when className is passed', () => {
-        expect(
-            render(<Submit className="start" />)
         ).toMatchSnapshot();
     });
 });
