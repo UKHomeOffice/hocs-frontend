eb0747f3dd96f6a58e27cfce32b1a15d2b7d602c
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-05-10 14:09:06 +0100

    Updated document-add to use providers

--- src/shared/common/forms/form-repository.jsx
+++ src/shared/common/forms/form-repository.jsx
@@ -8,7 +8,7 @@ import Radio from './radio-group.jsx';
 import DateInput from './date.jsx';
 import Checkbox from './checkbox-group.jsx';
 import TextArea from './text-area.jsx';
-import { WrappedDocumentAdd as AddDocument } from './composite/document-add.jsx';
+import AddDocument from './composite/document-add.jsx';
 import { WrappedEntityList as EntityList } from './composite/entity-list.jsx';
 import EntityManager from './composite/entity-manager.jsx';
 import { WrappedButton } from './button.jsx';
