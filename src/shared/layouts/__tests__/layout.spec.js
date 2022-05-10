28a66247e875a958f4649b718725d373c62d1e09
Robert Deniszczyc <robert.deniszczyc@digital.homeoffice.gov.uk>
2022-05-10 09:07:43 +0100

    Fixed wrapping on layout component

--- src/shared/layouts/__tests__/layout.spec.js
+++ src/shared/layouts/__tests__/layout.spec.js
@@ -1,22 +1,51 @@
 import React from 'react';
-import { Layout } from '../layout.jsx';
+import Layout from '../layout';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { ApplicationProvider } from '../../contexts/application';
+
+const MOCK_TRACK = jest.fn();
+
+const page = {
+    params: {
+        caseId: 'some_case_id',
+        stageId: 'some_stage_id',
+    }
+};
+
+const MOCK_CONFIG = {
+    track: MOCK_TRACK,
+    page
+};
 
 describe('Page layout component', () => {
     const mockLayout = {
-        header: {},
-        body: {},
+        header: null,
+        body: null,
         footer: {}
     };
 
     it('should render the footer when provided', () => {
         const mockLayoutWithFooter = { ...mockLayout, footer: { isVisible: true } };
+        const defaultProps = {
+            summary: {
+                type: 'default'
+            },
+            page: page,
+            track: MOCK_TRACK,
+            layout: mockLayoutWithFooter,
+            config: MOCK_CONFIG
+        };
+
         const wrapper = render(
-            <Layout layout={mockLayoutWithFooter} />,
-            { wrapper: MemoryRouter }
+            <ApplicationProvider config={{ ...MOCK_CONFIG, ...defaultProps }}>
+                <MemoryRouter>
+                    <Layout />
+                </MemoryRouter>
+            </ApplicationProvider>
         );
+
         expect(wrapper).toBeDefined();
         expect(screen.getByText('© Crown copyright')).toBeInTheDocument();
     });
