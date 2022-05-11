14bfdd1030eafa7cff00d0ddaac3f6467dff98bc
Liam MacPherson <liammacpherson1996@gmail.com>
2022-05-11 10:46:59 +0100

    HOCS-2083: change usage of Panel
    Multiple usages of the Panel for actions pass the title as the summary
    which breaks the Accessibility guidelines. All actions that pass title
    incorrectly have been changed.
    
    Also, includes a change that if the summary is not present then the
    `div` does not present as an empty element.

--- src/shared/common/forms/panel.jsx
+++ src/shared/common/forms/panel.jsx
@@ -4,6 +4,41 @@ import { ApplicationConsumer } from '../../contexts/application.jsx';
 import { Link } from 'react-router-dom';
 
 class Panel extends Component {
+
+    renderSummary(children) {
+        const summary = children?.summary;
+
+        if (!summary) {
+            return;
+        }
+
+        return (<div className="govuk-panel__body">
+            {children.summary}
+        </div>);
+    }
+
+    renderLink(children, csrf) {
+        const link = children?.link;
+
+        if (!link) {
+            return;
+        }
+
+        if (typeof link === 'string') {
+            return (<form action={`/search/reference?_csrf=${csrf}`} method='POST'
+                encType='multipart/form-data'>
+                <input className="govuk-input" id="case-reference" type="hidden" name="case-reference"
+                    value={children.link} />
+                <input className="govuk-button-panel--link" id="submit" type="submit"
+                    value={children.link} />
+            </form>);
+        } else if (typeof link === 'object') {
+            return (<Link to={children.link.href} className="govuk-input govuk-button-panel--link">
+                {children.link.label}
+            </Link>);
+        }
+    }
+
     /*
      * A visible container used on confirmation or results pages to highlight important content
      */
@@ -14,31 +49,16 @@ class Panel extends Component {
             <div>
                 <div className="govuk-panel govuk-panel--confirmation">
                     {title && <h1 className="govuk-panel__title">{title}</h1>}
-                    <div className="govuk-panel__body">{children.summary}
-                    </div>
+                    {this.renderSummary(children)}
                 </div>
-                {/* todo: why is the body not passed to the csrf parsing? */}
-                {children.link && typeof children.link === 'string' &&
-                    <form action={`/search/reference?_csrf=${csrf}`} method='POST'
-                        encType='multipart/form-data'>
-                        <input className="govuk-input" id="case-reference" type="hidden" name="case-reference"
-                            value={children.link} />
-                        <input className="govuk-button-panel--link" id="submit" type="submit"
-                            value={children.link} />
-                    </form>
-                }
-                {children.link && typeof children.link === 'object' &&
-                    <Link to={children.link.href} className="govuk-input govuk-button-panel--link">
-                        {children.link.label}
-                    </Link>
-                }
+                {this.renderLink(children, csrf)}
             </div>
         );
     }
 }
 
 Panel.propTypes = {
-    children: PropTypes.node,
+    children: PropTypes.node.isRequired,
     title: PropTypes.string,
     csrf: PropTypes.string
 };
