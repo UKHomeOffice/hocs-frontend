f57e0c962bc433fad20bbafa64a95eccd5dac4c8
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-08-09 12:58:53 +0100

    HOCS-5363: improve form embedded component
    Improve the form embedded component to now handle its own state
    representation of data, errors and submit handler (using action to
    designate API endpoint).
    
    This provided no functional difference to how the ex-gratia tab works at
    face value.

--- src/shared/common/components/tab-ex-gratia.jsx
+++ src/shared/common/components/tab-ex-gratia.jsx
@@ -5,8 +5,6 @@ import status from '../../helpers/api-status';
 import {
     updateApiStatus,
     unsetCaseData,
-    clearApiStatus,
-    updateCaseData,
 } from '../../contexts/actions/index.jsx';
 import axios from 'axios';
 import FormEmbeddedWrapped from '../forms/form-embedded-wrapped.jsx';
@@ -14,7 +12,6 @@ import FormEmbeddedWrapped from '../forms/form-embedded-wrapped.jsx';
 const TabExGratia = (props) => {
     const { dispatch, page } = useContext(Context);
     const [form, setForm] = useState(null);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         getForm();
@@ -32,61 +29,16 @@ const TabExGratia = (props) => {
             .then(() => dispatch(updateApiStatus(status.REQUEST_FORM_SUCCESS)));
     };
 
-    const submitHandler = (wrappedState, setWrappedState, actionDispatch) => {
-        return React.useCallback(e => {
-            e.preventDefault();
-            setWrappedState({ submittingForm: true });
-            const { screen } = props;
-
-            // eslint-disable-next-line no-undef
-            const formData = new FormData();
-            for (const [key, value] of Object.entries(wrappedState)) {
-                formData.append(key, value);
-            }
-            actionDispatch(updateApiStatus(status.UPDATE_CASE_DATA))
-                .then(() => {
-                    axios.post(`/api/case/${page.params.caseId}/stage/${page.params.stageId}/form/${screen}/data?type=EX_GRATIA_UPDATE`,
-                        formData,
-                        { headers: { 'Content-Type': 'multipart/form-data' } })
-                        .then((res) => {
-                            if (res.data.errors) {
-                                actionDispatch(updateApiStatus(status.SUBMIT_FORM_VALIDATION_ERROR))
-                                    .then(() => setError(res.data.errors))
-                                    .then(() => setWrappedState({ submittingForm: false }));
-                            } else {
-                                actionDispatch(updateApiStatus(status.UPDATE_CASE_DATA_SUCCESS))
-                                    .then(() => setError(null))
-                                    .then(() => actionDispatch(updateCaseData(wrappedState)))
-                                    .then(() => setWrappedState({ submittingForm: false }));
-                            }
-                        })
-                        .then(() => {
-                            actionDispatch(clearApiStatus());
-                        })
-                        .catch(() => {
-                            actionDispatch(updateApiStatus(status.UPDATE_CASE_DATA_FAILURE))
-                                .then(() => setWrappedState({ submittingForm: false }));
-                        });
-                })
-                .catch(() => {
-                    actionDispatch(updateApiStatus(status.REQUEST_CASE_DATA_FAILURE))
-                        .then(() => setWrappedState({ submittingForm: false }));
-                });
-        }, [wrappedState]);
-    };
-
     return (
         <Fragment>
             {form &&
             <Fragment>
                 <h2 className='govuk-heading-m'>{form.schema.title}</h2>
                 <FormEmbeddedWrapped
-                    schema={form.schema}
+                    schema={ form.schema }
                     fieldData={ form.data }
-                    errors={ error }
-                    action={`/case/${page.params.caseId}/stage/${page.params.stageId}/data`}
+                    action={`/api/case/${page.params.caseId}/stage/${page.params.stageId}/form/${props.screen}/data?type=EX_GRATIA_UPDATE`}
                     baseUrl={`/case/${page.params.caseId}/stage/${page.params.stageId}`}
-                    submitHandler={submitHandler}
                 />
             </Fragment>
             }
