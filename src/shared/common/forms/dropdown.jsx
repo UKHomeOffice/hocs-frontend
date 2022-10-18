d7078c04d7d33ec57b320b54961ed539bdb1dc20
Alex Whyte <alexander.whyte@digital.homeoffice.gov.uk>
2022-10-18 14:55:55 +0100

    fixed indent

--- src/shared/common/forms/dropdown.jsx
+++ src/shared/common/forms/dropdown.jsx
@@ -38,13 +38,11 @@ class Dropdown extends Component {
         if (conditionChoices) {
             for (var i = 0; i < conditionChoices.length; i++) {
                 const conditionPropertyValue = props.data[conditionChoices[i].conditionPropertyName];
-                if (conditionPropertyValue) {
-                    if (conditionChoices[i].conditionPropertyValue === conditionPropertyValue) {
-                        choicesToUse = conditionChoices[i].choices;
-                        break;
-                    }
-                    choicesToUse = [];
+                if (conditionPropertyValue && conditionChoices[i].conditionPropertyValue === conditionPropertyValue) {
+                    choicesToUse = conditionChoices[i].choices;
+                    break;
                 }
+                choicesToUse = [];
             }
         }
         return choicesToUse;
