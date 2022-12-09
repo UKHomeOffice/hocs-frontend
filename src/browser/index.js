36386429cfe9bc560f0542bdb8661b016d8dff91
Robert Deniszczyc <72561986+robertdeniszczyc2@users.noreply.github.com>
2022-12-09 10:03:44 +0000

    [HOCS-5349] Update to React 18 and Node 18 (#1208)
    Updates React to version 18, NodeJS 18, and also React Testing Library

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-app-polyfill/stable';
 import App from '../shared/index.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import '../styles/app.scss';
+
 /* eslint-disable no-undef*/
-hydrate(
-    <BrowserRouter>
-        <HelmetProvider>
-            <App config={window.__INITIAL_DATA__} />
-        </HelmetProvider>
-    </BrowserRouter>,
-    document.getElementById('app')
-);
+/* eslint-disable no-unused-vars*/
+const container = document.getElementById('app');
+const root = hydrateRoot(container, <BrowserRouter>
+    <HelmetProvider>
+        <App config={window.__INITIAL_DATA__} />
+    </HelmetProvider>
+</BrowserRouter>);
+/* eslint-enable no-unused-vars*/
 /* eslint-enable no-undef*/
+
