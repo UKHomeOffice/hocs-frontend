d3af59a27e6b76d92b8be8cb2e36f1e5dd3732d6
Vinoth Srinivasan <vinothon84@gmail.com>
2022-12-12 15:36:00 +0000

    HOCS-5566-Replace local variable with return statement

--- src/shared/common/components/workstack-tag.jsx
+++ src/shared/common/components/workstack-tag.jsx
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 const { fetchTagType } = require('../../../../server/config/tagType/tagType');
 
 export default function Tags(props) {
-    let tags = props.tags.map(tag => {
+    return props.tags.map(tag => {
         let tagDetails = fetchTagType(tag);
 
         return <span key={tag} className={tagDetails?.displayClass ?? 'govuk-tag govuk-!-margin-right-1'}>{tagDetails?.label ?? tag}</span>;
     });
-
-    return tags;
 }
 
 Tags.propTypes = {
