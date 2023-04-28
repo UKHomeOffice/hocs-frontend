9185345301a6939c019f7ae92e841551afd71be4
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-04-28 15:41:05 +0100

    Pass-through request id to function
    There was a missing pass-through of the requestId causing an non-allowed
    undefined header to attempt to be sent.

--- server/routes/search.js
+++ server/routes/search.js
@@ -11,7 +11,8 @@ router.get('/api/form/search', (req, res) => res.json(req.form));
 
 router.post(['/search/results', '/api/search/results'],
     async (req, res, next) => {
-        const formBuilder = await form({ submissionUrl: '/search/results', user: req.user, listService: req.listService });
+        const formBuilder = await form({ submissionUrl: '/search/results', user: req.user,
+            requestId: req.requestId, listService: req.listService });
         processRequestBody(formBuilder.getFields())(req, res, next);
     },
     getForm(form, { submissionUrl: '/search/results' }),
