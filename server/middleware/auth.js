0438c6a0a352b55c2a4f34bd4c7aa97d4651c3f4
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2023-05-09 12:42:27 +0100

    HOCS-6569: Add support for Tenant header

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -15,7 +15,8 @@ function authMiddleware(req, res, next) {
                 groups: req.get('X-Auth-Groups'),
                 roles: req.get('X-Auth-Roles'),
                 email: req.get('X-Auth-Email'),
-                uuid: req.get('X-Auth-Subject')
+                uuid: req.get('X-Auth-Subject'),
+                tenant: req.get('X-Auth-Tenant')
             });
         }
         return next();
