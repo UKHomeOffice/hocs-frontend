e880ac331c44f87dd00af1754f9ecb6a705b984f
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-05-09 11:32:00 +0100

    Add new document labels
    Add the new document labels for `AWAITING_MALWARE_SCAN`,
    `FAILED_MALWARE_SCAN`, and `AWAITING_CONVERSION`. This change also
    brings the wording in line with hocs-docs.

--- server/lists/adapters/__tests__/documents.spec.js
+++ server/lists/adapters/__tests__/documents.spec.js
@@ -15,8 +15,10 @@ describe('Documents Adapter', () => {
                 { created: '02-01-2019', displayName: 'Document A', type: 'type1', uuid: 1, status: 'PENDING', hasPdf: true, hasOriginalFile: true },
                 { created: '01-01-2019', displayName: 'Document B', type: 'type1', uuid: 2, status: 'FAILED_CONVERSION', hasPdf: false, hasOriginalFile: true },
                 { created: '03-01-2019', displayName: 'Document C', type: 'type2', uuid: 3, status: 'FAILED_VIRUS', labels: [], hasPdf: false, hasOriginalFile: false },
-                { created: '01-01-2019', displayName: 'Document D', type: 'type2', uuid: 4, status: 'INVALID', labels: undefined, hasPdf: false, hasOriginalFile: false },
-                { created: '01-01-2019', displayName: 'Document E', type: 'type1', uuid: 5, status: 'PENDING', labels: ['Test Label'], hasPdf: false, hasOriginalFile: false }
+                { created: '03-01-2019', displayName: 'Document D', type: 'type2', uuid: 3, status: 'FAILED_MALWARE_SCAN', labels: [], hasPdf: false, hasOriginalFile: false },
+                { created: '01-01-2019', displayName: 'Document E', type: 'type2', uuid: 4, status: 'INVALID', labels: undefined, hasPdf: false, hasOriginalFile: false },
+                { created: '01-01-2019', displayName: 'Document F', type: 'type1', uuid: 5, status: 'AWAITING_MALWARE_SCAN', labels: ['Test Label'], hasPdf: false, hasOriginalFile: false },
+                { created: '01-01-2019', displayName: 'Document G', type: 'type1', uuid: 5, status: 'AWAITING_CONVERSION', labels: ['Test Label'], hasPdf: false, hasOriginalFile: false }
             ],
             documentTags: ['type1', 'type2', 'type3']
         };
