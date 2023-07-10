b9ae2c70f2c1e288bce3c9204f09a23445be597c
Joseph Rogerson <joseph.rogerson@digital.homeoffice.gov.uk>
2023-07-10 15:38:44 +0100

    HOCS-6598: Restrict tabs based on role

--- server/config/caseTabs/caseTabs.js
+++ server/config/caseTabs/caseTabs.js
@@ -1,9 +1,15 @@
 const config = require('./config.json');
+const User = require('../../models/user');
 
 const fetchCaseTabsForCaseType = (caseType) => {
     return config[caseType] ?? [];
 };
 
+const fetchCaseTabsForCaseTypeAndUser = (caseType, user) => {
+    return fetchCaseTabsForCaseType(caseType).filter(caseTab => caseTab.required_role ? User.hasRole(user, caseTab.required_role) : true);
+};
+
 module.exports = {
-    fetchCaseTabsForCaseType
+    fetchCaseTabsForCaseType,
+    fetchCaseTabsForCaseTypeAndUser
 };
